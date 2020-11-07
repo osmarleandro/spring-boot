460772399947ba648b7ad77c382aae48be439c78
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 13:58:27 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.health,HealthIndicatorTests,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/HealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/HealthIndicatorTests.java
@@ -25,7 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Phillip Webb
  */
-class HealthIndicatorTests {
+class HealthIndicatorTests implements IHealthIndicatorTests {
 
 	private final HealthIndicator indicator = () -> Health.up().withDetail("spring", "boot").build();
 
