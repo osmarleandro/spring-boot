e0e4a71af99b00b6c6a3584e962f7a626e3f1280
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 07:43:25 -0300

    spring-boot-actuator,org.springframework.boot.actuate.audit.listener,AuditListenerTests,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/audit/listener/AuditListenerTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/audit/listener/AuditListenerTests.java
@@ -31,7 +31,7 @@ import static org.mockito.Mockito.verify;
  *
  * @author Phillip Webb
  */
-class AuditListenerTests {
+class AuditListenerTests implements IAuditListenerTests {
 
 	@Test
 	void testStoredEvents() {
