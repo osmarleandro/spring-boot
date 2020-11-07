ec6bc79b453c33907852c134569044dc6d60afc7
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 14:05:57 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.health,SimpleHttpCodeStatusMapperTests,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/SimpleHttpCodeStatusMapperTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/SimpleHttpCodeStatusMapperTests.java
@@ -30,7 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Phillip Webb
  */
-class SimpleHttpCodeStatusMapperTests {
+class SimpleHttpCodeStatusMapperTests implements ISimpleHttpCodeStatusMapperTests {
 
 	@Test
 	void createWhenMappingsAreNullUsesDefaultMappings() {
