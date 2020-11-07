28fbfdf5fe1974914964d841a6cfac78088882b6
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 07:47:14 -0300

    spring-boot-actuator,org.springframework.boot.actuate.cassandra,CassandraHealthIndicatorTests,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/cassandra/CassandraHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/cassandra/CassandraHealthIndicatorTests.java
@@ -39,7 +39,7 @@ import static org.mockito.Mockito.mock;
  * @author Stephane Nicoll
  */
 @Deprecated
-class CassandraHealthIndicatorTests {
+class CassandraHealthIndicatorTests implements ICassandraHealthIndicatorTests {
 
 	@Test
 	void createWhenCassandraOperationsIsNullShouldThrowException() {
