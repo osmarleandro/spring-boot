0d70ee24acdbbe6b6fff92b1b3e0b294e22b6f10
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 13:43:30 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.endpoint.web.annotation,AbstractWebEndpointIntegrationTests,NA,ExtractInterface,OK,NA

---
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/annotation/IAbstractWebEndpointIntegrationTests.java
@@ -0,0 +1,5 @@
+package org.springframework.boot.actuate.endpoint.web.annotation;
+
+public interface IAbstractWebEndpointIntegrationTests<T extends ConfigurableApplicationContext & AnnotationConfigRegistry> {
+
+}
