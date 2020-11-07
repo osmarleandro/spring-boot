8d5320f52775cbfa16450ccc21c4cb62314e7bcf
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 11:36:22 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.metrics.cache,RedisCacheMeterBinderProvider,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/cache/CacheMeterBinderProvidersConfiguration.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/metrics/cache/CacheMeterBinderProvidersConfiguration.java
@@ -25,6 +25,7 @@ import org.springframework.boot.actuate.metrics.cache.CacheMeterBinderProvider;
 import org.springframework.boot.actuate.metrics.cache.CaffeineCacheMeterBinderProvider;
 import org.springframework.boot.actuate.metrics.cache.EhCache2CacheMeterBinderProvider;
 import org.springframework.boot.actuate.metrics.cache.HazelcastCacheMeterBinderProvider;
+import org.springframework.boot.actuate.metrics.cache.IRedisCacheMeterBinderProvider;
 import org.springframework.boot.actuate.metrics.cache.JCacheCacheMeterBinderProvider;
 import org.springframework.boot.actuate.metrics.cache.RedisCacheMeterBinderProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -93,7 +94,7 @@ class CacheMeterBinderProvidersConfiguration {
 	static class RedisCacheMeterBinderProviderConfiguration {
 
 		@Bean
-		RedisCacheMeterBinderProvider redisCacheMeterBinderProvider() {
+		IRedisCacheMeterBinderProvider redisCacheMeterBinderProvider() {
 			return new RedisCacheMeterBinderProvider();
 		}
 
