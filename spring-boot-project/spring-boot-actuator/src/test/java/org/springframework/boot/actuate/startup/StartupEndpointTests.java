ea82d1ada4c445322f78d71f5d1290b2986474e8
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 08:28:53 -0300

    spring-boot-actuator,org.springframework.boot.actuate.startup,StartupEndpointTests,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/startup/StartupEndpointTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/startup/StartupEndpointTests.java
@@ -31,7 +31,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Brian Clozel
  */
-class StartupEndpointTests {
+class StartupEndpointTests implements IStartupEndpointTests {
 
 	@Test
 	void startupEventsAreFound() {
