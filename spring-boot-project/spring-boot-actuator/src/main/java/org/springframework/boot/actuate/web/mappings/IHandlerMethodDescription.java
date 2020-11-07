21b1908ab575ffc8f5ce6411e063d5850cb171cb
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 07:23:22 -0300

    spring-boot-actuator,org.springframework.boot.actuate.web.mappings,HandlerMethodDescription,NA,ExtractInterface,OK,NA

---
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/mappings/IHandlerMethodDescription.java
@@ -0,0 +1,11 @@
+package org.springframework.boot.actuate.web.mappings;
+
+public interface IHandlerMethodDescription {
+
+	String getName();
+
+	String getDescriptor();
+
+	String getClassName();
+
+}
