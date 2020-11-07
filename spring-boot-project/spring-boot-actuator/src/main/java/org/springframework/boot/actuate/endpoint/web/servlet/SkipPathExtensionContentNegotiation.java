6ab74fd26d9adbf66f84a4f21f220070726bc425
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 10:20:29 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.endpoint.web.servlet,SkipPathExtensionContentNegotiation,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/servlet/SkipPathExtensionContentNegotiation.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/servlet/SkipPathExtensionContentNegotiation.java
@@ -28,7 +28,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
  *
  * @author Phillip Webb
  */
-final class SkipPathExtensionContentNegotiation implements HandlerInterceptor {
+final class SkipPathExtensionContentNegotiation implements HandlerInterceptor, ISkipPathExtensionContentNegotiation {
 
 	@SuppressWarnings("deprecation")
 	private static final String SKIP_ATTRIBUTE = org.springframework.web.accept.PathExtensionContentNegotiationStrategy.class
