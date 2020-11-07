be76b765ad3864c40df074621c1269f2964499d2
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 07:55:25 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.annotation,DiscovererEndpointFilterTests,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/annotation/DiscovererEndpointFilterTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/annotation/DiscovererEndpointFilterTests.java
@@ -37,7 +37,7 @@ import static org.mockito.Mockito.mock;
  *
  * @author Phillip Webb
  */
-class DiscovererEndpointFilterTests {
+class DiscovererEndpointFilterTests implements IDiscovererEndpointFilterTests {
 
 	@Test
 	void createWhenDiscovererIsNullShouldThrowException() {
