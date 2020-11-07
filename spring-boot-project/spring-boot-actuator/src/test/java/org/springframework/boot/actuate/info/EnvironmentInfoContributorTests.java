2908805f2f7944ee8a3e0f7fbfc1d3094e50f030
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 14:14:32 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.info,EnvironmentInfoContributorTests,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/info/EnvironmentInfoContributorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/info/EnvironmentInfoContributorTests.java
@@ -32,7 +32,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Stephane Nicoll
  */
-class EnvironmentInfoContributorTests {
+class EnvironmentInfoContributorTests implements IEnvironmentInfoContributorTests {
 
 	private final StandardEnvironment environment = new StandardEnvironment();
 
