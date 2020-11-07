be7f380982f2e69d94b73072ec85ab35cd105847
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-07 08:02:07 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.web,EndpointMappingTests,NA,ExtractInterface,OK,NA

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/EndpointMappingTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/EndpointMappingTests.java
@@ -25,7 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
-class EndpointMappingTests {
+class EndpointMappingTests implements IEndpointMappingTests {
 
 	@Test
 	void normalizationTurnsASlashIntoAnEmptyString() {
