780ea7023bc8df1f08708aab089058ee677ec693
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 08:20:07 -0300

    spring-boot-actuator,org.springframework.boot.actuate.metrics.web.servlet,WebMvcMetricsFilterTests,subclassedTimedMethod#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/WebMvcMetricsFilterTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/WebMvcMetricsFilterTests.java
@@ -135,13 +135,6 @@ class WebMvcMetricsFilterTests {
 	}
 
 	@Test
-	void subclassedTimedMethod() throws Exception {
-		this.mvc.perform(get("/api/c1/metaTimed/10")).andExpect(status().isOk());
-		assertThat(this.registry.get("http.server.requests").tags("status", "200", "uri", "/api/c1/metaTimed/{id}")
-				.timer().count()).isEqualTo(1L);
-	}
-
-	@Test
 	void untimedMethod() throws Exception {
 		this.mvc.perform(get("/api/c1/untimed/10")).andExpect(status().isOk());
 		assertThat(this.registry.find("http.server.requests").tags("uri", "/api/c1/untimed/10").timer()).isNull();
