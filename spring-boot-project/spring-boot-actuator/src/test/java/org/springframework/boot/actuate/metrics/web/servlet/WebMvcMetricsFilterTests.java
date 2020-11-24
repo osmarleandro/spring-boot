6437c3e8368830f171f6ff3a9387e43080122e22
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 08:22:37 -0300

    spring-boot-actuator,org.springframework.boot.actuate.metrics.web.servlet,WebMvcMetricsFilterTests,asyncCompletableFutureRequest#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/WebMvcMetricsFilterTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/WebMvcMetricsFilterTests.java
@@ -240,28 +240,6 @@ class WebMvcMetricsFilterTests {
 	}
 
 	@Test
-	void asyncCompletableFutureRequest() throws Exception {
-		AtomicReference<MvcResult> result = new AtomicReference<>();
-		Thread backgroundRequest = new Thread(() -> {
-			try {
-				result.set(this.mvc.perform(get("/api/c1/completableFuture/{id}", 1))
-						.andExpect(request().asyncStarted()).andReturn());
-			}
-			catch (Exception ex) {
-				fail("Failed to execute async request", ex);
-			}
-		});
-		backgroundRequest.start();
-		this.completableFutureBarrier.await();
-		MockClock.clock(this.registry).add(Duration.ofSeconds(2));
-		this.completableFutureBarrier.await();
-		backgroundRequest.join();
-		this.mvc.perform(asyncDispatch(result.get())).andExpect(status().isOk());
-		assertThat(this.registry.get("http.server.requests").tags("uri", "/api/c1/completableFuture/{id}").timer()
-				.totalTime(TimeUnit.SECONDS)).isEqualTo(2);
-	}
-
-	@Test
 	void endpointThrowsError() throws Exception {
 		this.mvc.perform(get("/api/c1/error/10")).andExpect(status().is4xxClientError());
 		assertThat(this.registry.get("http.server.requests").tags("status", "422").timer().count()).isEqualTo(1L);
