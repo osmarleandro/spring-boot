4cb75a841f182acf2fc5a27836f9ca0f97a5b3fc
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 09:39:24 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.endpoint.annotation,AbstractDiscoveredEndpoint,appendFields#QToStringCreator,InlineMethod,OK,false,false,1

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/annotation/AbstractDiscoveredEndpoint.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/annotation/AbstractDiscoveredEndpoint.java
@@ -71,11 +71,7 @@ public abstract class AbstractDiscoveredEndpoint<O extends Operation> extends Ab
 	public String toString() {
 		ToStringCreator creator = new ToStringCreator(this).append("discoverer", this.discoverer.getClass().getName())
 				.append("endpointBean", this.endpointBean.getClass().getName());
-		appendFields(creator);
 		return creator.toString();
-	}
-
-	protected void appendFields(ToStringCreator creator) {
 	}
 
 }
