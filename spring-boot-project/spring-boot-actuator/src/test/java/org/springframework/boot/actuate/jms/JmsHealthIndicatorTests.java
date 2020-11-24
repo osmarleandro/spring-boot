bcc24382666795d7164ffb4ba9466a6bfdde882f
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 07:42:11 -0300

    spring-boot-actuator,org.springframework.boot.actuate.jms,JmsHealthIndicatorTests,jmsBrokerCouldNotRetrieveProviderMetadata#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/jms/JmsHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/jms/JmsHealthIndicatorTests.java
@@ -69,21 +69,6 @@ class JmsHealthIndicatorTests {
 	}
 
 	@Test
-	void jmsBrokerCouldNotRetrieveProviderMetadata() throws JMSException {
-		ConnectionMetaData connectionMetaData = mock(ConnectionMetaData.class);
-		given(connectionMetaData.getJMSProviderName()).willThrow(new JMSException("test", "123"));
-		Connection connection = mock(Connection.class);
-		given(connection.getMetaData()).willReturn(connectionMetaData);
-		ConnectionFactory connectionFactory = mock(ConnectionFactory.class);
-		given(connectionFactory.createConnection()).willReturn(connection);
-		JmsHealthIndicator indicator = new JmsHealthIndicator(connectionFactory);
-		Health health = indicator.health();
-		assertThat(health.getStatus()).isEqualTo(Status.DOWN);
-		assertThat(health.getDetails().get("provider")).isNull();
-		verify(connection, times(1)).close();
-	}
-
-	@Test
 	void jmsBrokerUsesFailover() throws JMSException {
 		ConnectionFactory connectionFactory = mock(ConnectionFactory.class);
 		ConnectionMetaData connectionMetaData = mock(ConnectionMetaData.class);
