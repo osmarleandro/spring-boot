ac822df44d49341d97574c637fcab4faa6e0573d
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 10:37:09 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.endpoint.web,EndpointServlet,withLoadOnStartup#I,InlineMethod,OK,false,false,2

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/ServletEndpointRegistrarTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/ServletEndpointRegistrarTests.java
@@ -110,7 +110,8 @@ class ServletEndpointRegistrarTests {
 	@Test
 	void onStartupWhenHasLoadOnStartupShouldRegisterLoadOnStartup() throws Exception {
 		given(this.servletContext.addServlet(any(String.class), any(Servlet.class))).willReturn(this.dynamic);
-		ExposableServletEndpoint endpoint = mockEndpoint(new EndpointServlet(TestServlet.class).withLoadOnStartup(7));
+		EndpointServlet r = new EndpointServlet(TestServlet.class);
+		ExposableServletEndpoint endpoint = mockEndpoint(new EndpointServlet(r.servlet, r.initParameters, 7));
 		ServletEndpointRegistrar registrar = new ServletEndpointRegistrar("/actuator", Collections.singleton(endpoint));
 		registrar.onStartup(this.servletContext);
 		verify(this.dynamic).setLoadOnStartup(7);
