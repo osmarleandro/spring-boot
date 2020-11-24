683b07ed64f247c6f15e7764480f2127015a14a9
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 20:42:24 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.mongo,MongoHealthIndicatorTests,mongoIsDown#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/mongo/MongoHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/mongo/MongoHealthIndicatorTests.java
@@ -50,15 +50,4 @@ class MongoHealthIndicatorTests {
 		verify(mongoTemplate).executeCommand("{ buildInfo: 1 }");
 	}
 
-	@Test
-	void mongoIsDown() {
-		MongoTemplate mongoTemplate = mock(MongoTemplate.class);
-		given(mongoTemplate.executeCommand("{ buildInfo: 1 }")).willThrow(new MongoException("Connection failed"));
-		MongoHealthIndicator healthIndicator = new MongoHealthIndicator(mongoTemplate);
-		Health health = healthIndicator.health();
-		assertThat(health.getStatus()).isEqualTo(Status.DOWN);
-		assertThat((String) health.getDetails().get("error")).contains("Connection failed");
-		verify(mongoTemplate).executeCommand("{ buildInfo: 1 }");
-	}
-
 }
