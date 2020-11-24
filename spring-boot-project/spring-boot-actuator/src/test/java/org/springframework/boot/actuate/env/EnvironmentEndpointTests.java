df3440af981fffed8ff67c5b3c548477ba8a191c
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 07:04:56 -0300

    spring-boot-actuator,org.springframework.boot.actuate.env,EnvironmentEndpointTests,propertyWithPlaceholderNotResolved#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/env/EnvironmentEndpointTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/env/EnvironmentEndpointTests.java
@@ -167,15 +167,6 @@ class EnvironmentEndpointTests {
 	}
 
 	@Test
-	void propertyWithPlaceholderNotResolved() {
-		ConfigurableEnvironment environment = emptyEnvironment();
-		TestPropertyValues.of("my.foo: ${bar.blah}").applyTo(environment);
-		EnvironmentDescriptor descriptor = new EnvironmentEndpoint(environment).environment(null);
-		assertThat(propertySources(descriptor).get("test").getProperties().get("my.foo").getValue())
-				.isEqualTo("${bar.blah}");
-	}
-
-	@Test
 	void propertyWithSensitivePlaceholderResolved() {
 		ConfigurableEnvironment environment = emptyEnvironment();
 		TestPropertyValues.of("my.foo: http://${bar.password}://hello", "bar.password: hello").applyTo(environment);
