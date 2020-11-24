ec888fe3556b0b3fc3bcc85eeed082efde97cc0f
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 08:25:17 -0300

    spring-boot-actuator,org.springframework.boot.actuate.metrics.web.servlet,LongTaskTimingHandlerInterceptorTests,asyncCallableRequest#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/LongTaskTimingHandlerInterceptorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/LongTaskTimingHandlerInterceptorTests.java
@@ -94,29 +94,6 @@ class LongTaskTimingHandlerInterceptorTests {
 		assertThat(this.registry.get("my.long.request.exception").longTaskTimer().activeTasks()).isEqualTo(0);
 	}
 
-	@Test
-	void asyncCallableRequest() throws Exception {
-		AtomicReference<MvcResult> result = new AtomicReference<>();
-		Thread backgroundRequest = new Thread(() -> {
-			try {
-				result.set(
-						this.mvc.perform(get("/api/c1/callable/10")).andExpect(request().asyncStarted()).andReturn());
-			}
-			catch (Exception ex) {
-				fail("Failed to execute async request", ex);
-			}
-		});
-		backgroundRequest.start();
-		this.callableBarrier.await();
-		assertThat(this.registry.get("my.long.request").tags("region", "test").longTaskTimer().activeTasks())
-				.isEqualTo(1);
-		this.callableBarrier.await();
-		backgroundRequest.join();
-		this.mvc.perform(asyncDispatch(result.get())).andExpect(status().isOk());
-		assertThat(this.registry.get("my.long.request").tags("region", "test").longTaskTimer().activeTasks())
-				.isEqualTo(0);
-	}
-
 	@Configuration(proxyBeanMethods = false)
 	@EnableWebMvc
 	@Import(Controller1.class)
