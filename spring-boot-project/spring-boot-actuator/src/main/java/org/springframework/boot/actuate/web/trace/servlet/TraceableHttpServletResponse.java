b4a0851289ec7fcf3221e3dc64e53b1ea7740609
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 05:03:21 -0300

    spring-boot-actuator,org.springframework.boot.actuate.web.trace.servlet,TraceableHttpServletResponse,getHeaders#,InlineMethod,BUILDFAILED,false,true,3

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/trace/servlet/TraceableHttpServletResponse.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/trace/servlet/TraceableHttpServletResponse.java
@@ -43,11 +43,6 @@ final class TraceableHttpServletResponse implements TraceableResponse {
 		return this.delegate.getStatus();
 	}
 
-	@Override
-	public Map<String, List<String>> getHeaders() {
-		return extractHeaders();
-	}
-
 	private Map<String, List<String>> extractHeaders() {
 		Map<String, List<String>> headers = new LinkedHashMap<>();
 		for (String name : this.delegate.getHeaderNames()) {
