8ef0ba8b67f059b118fa202475b6f9b256c5c08c
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 19:17:34 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.health,SimpleStatusAggregatorTests,getAggregateStatusWhenUsingNewDefaultOrder#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/SimpleStatusAggregatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/SimpleStatusAggregatorTests.java
@@ -36,13 +36,6 @@ class SimpleStatusAggregatorTests {
 	}
 
 	@Test
-	void getAggregateStatusWhenUsingNewDefaultOrder() {
-		SimpleStatusAggregator aggregator = new SimpleStatusAggregator();
-		Status status = aggregator.getAggregateStatus(Status.DOWN, Status.UP, Status.UNKNOWN, Status.OUT_OF_SERVICE);
-		assertThat(status).isEqualTo(Status.DOWN);
-	}
-
-	@Test
 	void getAggregateStatusWhenUsingCustomOrder() {
 		SimpleStatusAggregator aggregator = new SimpleStatusAggregator(Status.UNKNOWN, Status.UP, Status.OUT_OF_SERVICE,
 				Status.DOWN);
