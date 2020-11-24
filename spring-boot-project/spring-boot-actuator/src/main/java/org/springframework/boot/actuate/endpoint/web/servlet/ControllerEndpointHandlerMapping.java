231d7b42d8be405f38275227cffad3fdb3167e33
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 02:48:33 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.web.servlet,ControllerEndpointHandlerMapping,getEndpointMappedPattern#QExposableControllerEndpoint;QString,InlineMethod,OK,false,false,1

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/servlet/ControllerEndpointHandlerMapping.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/servlet/ControllerEndpointHandlerMapping.java
@@ -95,12 +95,8 @@ public class ControllerEndpointHandlerMapping extends RequestMappingHandlerMappi
 			patterns = Collections.singleton("");
 		}
 		String[] endpointMappedPatterns = patterns.stream()
-				.map((pattern) -> getEndpointMappedPattern(endpoint, pattern)).toArray(String[]::new);
+				.map((pattern) -> this.endpointMapping.createSubPath(endpoint.getRootPath() + pattern)).toArray(String[]::new);
 		return withNewPatterns(mapping, endpointMappedPatterns);
-	}
-
-	private String getEndpointMappedPattern(ExposableControllerEndpoint endpoint, String pattern) {
-		return this.endpointMapping.createSubPath(endpoint.getRootPath() + pattern);
 	}
 
 	@SuppressWarnings("deprecation")
