351dbabdf0065780b782c926a0b622832b924b74
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 19:05:31 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.health,CompositeReactiveHealthContributorTests,fromMapWithAdapterReturnsCompositeReactiveHealthContributorMapAdapter#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/CompositeReactiveHealthContributorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/CompositeReactiveHealthContributorTests.java
@@ -43,18 +43,4 @@ class CompositeReactiveHealthContributorTests {
 		assertThat(namedContributor.getContributor()).isSameAs(indicator);
 	}
 
-	@Test
-	void fromMapWithAdapterReturnsCompositeReactiveHealthContributorMapAdapter() {
-		Map<String, ReactiveHealthContributor> map = new LinkedHashMap<>();
-		ReactiveHealthIndicator downIndicator = () -> Mono.just(Health.down().build());
-		ReactiveHealthIndicator upIndicator = () -> Mono.just(Health.up().build());
-		map.put("test", downIndicator);
-		CompositeReactiveHealthContributor composite = CompositeReactiveHealthContributor.fromMap(map,
-				(value) -> upIndicator);
-		assertThat(composite).isInstanceOf(CompositeReactiveHealthContributorMapAdapter.class);
-		NamedContributor<ReactiveHealthContributor> namedContributor = composite.iterator().next();
-		assertThat(namedContributor.getName()).isEqualTo("test");
-		assertThat(namedContributor.getContributor()).isSameAs(upIndicator);
-	}
-
 }
