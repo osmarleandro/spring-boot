d1de6f6a535b066a5077572b5cc3f2d165f5b726
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 19:09:50 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.health,HealthEndpointSupportTests,getHealthWhenAlwaysShowIsTrueShowsDetails#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/HealthEndpointSupportTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/HealthEndpointSupportTests.java
@@ -155,15 +155,6 @@ abstract class HealthEndpointSupportTests<R extends ContributorRegistry<C>, C, T
 	}
 
 	@Test
-	void getHealthWhenAlwaysShowIsTrueShowsDetails() {
-		this.primaryGroup.setShowDetails(false);
-		this.registry.registerContributor("test", createContributor(this.up));
-		HealthResult<T> result = create(this.registry, this.groups).getHealth(ApiVersion.V3, SecurityContext.NONE, true,
-				"test");
-		assertThat(((Health) getHealth(result)).getDetails()).containsEntry("spring", "boot");
-	}
-
-	@Test
 	void getHealthWhenCompositeReturnsAggregateResult() {
 		Map<String, C> contributors = new LinkedHashMap<>();
 		contributors.put("a", createContributor(this.up));
