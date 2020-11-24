f6ddfcb1fc3a1b56ac94b54f3b2c15bb0999d9da
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 20:33:00 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.metrics.web.servlet,WebMvcMetricsFilterTests,timedMethod#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/WebMvcMetricsFilterTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/WebMvcMetricsFilterTests.java
@@ -128,13 +128,6 @@ class WebMvcMetricsFilterTests {
 	}
 
 	@Test
-	void timedMethod() throws Exception {
-		this.mvc.perform(get("/api/c1/10")).andExpect(status().isOk());
-		assertThat(this.registry.get("http.server.requests")
-				.tags("status", "200", "uri", "/api/c1/{id}", "public", "true").timer().count()).isEqualTo(1);
-	}
-
-	@Test
 	void subclassedTimedMethod() throws Exception {
 		this.mvc.perform(get("/api/c1/metaTimed/10")).andExpect(status().isOk());
 		assertThat(this.registry.get("http.server.requests").tags("status", "200", "uri", "/api/c1/metaTimed/{id}")
