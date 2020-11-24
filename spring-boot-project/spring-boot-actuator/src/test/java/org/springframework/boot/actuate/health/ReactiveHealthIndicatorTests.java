865a93728454291bed3e9980b2747b7a9ef080a4
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 18:41:32 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.health,ReactiveHealthIndicatorTests,getHealthWhenIncludeDetailsIsTrueReturnsHealthWithDetails#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/ReactiveHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/ReactiveHealthIndicatorTests.java
@@ -31,13 +31,6 @@ class ReactiveHealthIndicatorTests {
 	private final ReactiveHealthIndicator indicator = () -> Mono.just(Health.up().withDetail("spring", "boot").build());
 
 	@Test
-	void getHealthWhenIncludeDetailsIsTrueReturnsHealthWithDetails() {
-		Health health = this.indicator.getHealth(true).block();
-		assertThat(health.getStatus()).isEqualTo(Status.UP);
-		assertThat(health.getDetails()).containsEntry("spring", "boot");
-	}
-
-	@Test
 	void getHealthWhenIncludeDetailsIsFalseReturnsHealthWithoutDetails() {
 		Health health = this.indicator.getHealth(false).block();
 		assertThat(health.getStatus()).isEqualTo(Status.UP);
