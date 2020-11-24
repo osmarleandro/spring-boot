18a6be67b37c24307b6b5c9413f7c3ca48947956
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 08:41:36 -0300

    spring-boot-actuator,org.springframework.boot.actuate.trace.http,HttpExchangeTracerTests,mixedCaseSetCookieHeaderIsNotIncludedByDefault#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/trace/http/HttpExchangeTracerTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/trace/http/HttpExchangeTracerTests.java
@@ -197,15 +197,6 @@ class HttpExchangeTracerTests {
 	}
 
 	@Test
-	void mixedCaseSetCookieHeaderIsNotIncludedByDefault() {
-		HttpTrace trace = new HttpTrace(createRequest());
-		new HttpExchangeTracer(EnumSet.of(Include.RESPONSE_HEADERS)).sendingResponse(trace,
-				createResponse(Collections.singletonMap(mixedCase(HttpHeaders.SET_COOKIE), Arrays.asList("test=test"))),
-				null, null);
-		assertThat(trace.getResponse().getHeaders()).isEmpty();
-	}
-
-	@Test
 	void setCookieHeaderCanBeIncluded() {
 		HttpTrace trace = new HttpTrace(createRequest());
 		new HttpExchangeTracer(EnumSet.of(Include.RESPONSE_HEADERS, Include.COOKIE_HEADERS)).sendingResponse(trace,
