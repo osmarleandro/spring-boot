8f9a916eb7f14b1a47b2684082a41cc96a78f6fc
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 08:44:29 -0300

    spring-boot-actuator,org.springframework.boot.actuate.trace.http,InMemoryHttpTraceRepositoryTests,reverseFalse#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/trace/http/InMemoryHttpTraceRepositoryTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/trace/http/InMemoryHttpTraceRepositoryTests.java
@@ -46,19 +46,6 @@ class InMemoryHttpTraceRepositoryTests {
 		assertThat(traces.get(1).getRequest().getMethod()).isEqualTo("POST");
 	}
 
-	@Test
-	void reverseFalse() {
-		this.repository.setReverse(false);
-		this.repository.setCapacity(2);
-		this.repository.add(new HttpTrace(createRequest("GET")));
-		this.repository.add(new HttpTrace(createRequest("POST")));
-		this.repository.add(new HttpTrace(createRequest("DELETE")));
-		List<HttpTrace> traces = this.repository.findAll();
-		assertThat(traces).hasSize(2);
-		assertThat(traces.get(0).getRequest().getMethod()).isEqualTo("POST");
-		assertThat(traces.get(1).getRequest().getMethod()).isEqualTo("DELETE");
-	}
-
 	private TraceableRequest createRequest(String method) {
 		TraceableRequest request = mock(TraceableRequest.class);
 		given(request.getMethod()).willReturn(method);
