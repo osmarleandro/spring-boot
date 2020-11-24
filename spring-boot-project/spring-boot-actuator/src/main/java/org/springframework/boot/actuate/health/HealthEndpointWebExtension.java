dbda853b2d99ea5080bd1a6f76f0cc1502d28131
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 12:22:13 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.health,HealthEndpointWebExtension,aggregateContributions#QApiVersion;QMapQStringQHealthComponent;QStatusAggregator;Z;QSetQString,InlineMethod,BUILDFAILED,false,true,1

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/health/HealthEndpointWebExtension.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/health/HealthEndpointWebExtension.java
@@ -85,10 +85,4 @@ public class HealthEndpointWebExtension extends HealthEndpointSupport<HealthCont
 		return ((HealthIndicator) contributor).getHealth(includeDetails);
 	}
 
-	@Override
-	protected HealthComponent aggregateContributions(ApiVersion apiVersion, Map<String, HealthComponent> contributions,
-			StatusAggregator statusAggregator, boolean showComponents, Set<String> groupNames) {
-		return getCompositeHealth(apiVersion, contributions, statusAggregator, showComponents, groupNames);
-	}
-
 }
