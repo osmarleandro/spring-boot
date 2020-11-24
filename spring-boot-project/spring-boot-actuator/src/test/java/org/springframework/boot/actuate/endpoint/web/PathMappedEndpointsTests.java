dd51caba979bb4e61e50d7c3e326b93626b4e526
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 10:43:18 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.endpoint.web,PathMappedEndpoints,stream#,InlineMethod,OK,false,false,3

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/PathMappedEndpointsTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/PathMappedEndpointsTests.java
@@ -63,8 +63,8 @@ class PathMappedEndpointsTests {
 	@Test
 	void streamShouldReturnPathMappedEndpoints() {
 		PathMappedEndpoints mapped = createTestMapped(null);
-		assertThat(mapped.stream()).hasSize(2);
-		assertThat(mapped.stream()).extracting("endpointId").containsExactly(EndpointId.of("e2"), EndpointId.of("e3"));
+		assertThat(mapped.endpoints.values().stream()).hasSize(2);
+		assertThat(mapped.endpoints.values().stream()).extracting("endpointId").containsExactly(EndpointId.of("e2"), EndpointId.of("e3"));
 	}
 
 	@Test
