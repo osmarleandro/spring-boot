738cca2d3fa7868a0b2018e01a3a61958aa301b8
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 18:06:58 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.endpoint.web.servlet,ControllerEndpointHandlerMappingTests,handlerOf#QObject;QString,InlineMethod,OK,false,false,3

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/servlet/ControllerEndpointHandlerMappingTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/servlet/ControllerEndpointHandlerMappingTests.java
@@ -52,10 +52,12 @@ class ControllerEndpointHandlerMappingTests {
 		ExposableControllerEndpoint first = firstEndpoint();
 		ExposableControllerEndpoint second = secondEndpoint();
 		ControllerEndpointHandlerMapping mapping = createMapping("", first, second);
+		Object source = first.getController();
 		assertThat(mapping.getHandler(request("GET", "/first")).getHandler())
-				.isEqualTo(handlerOf(first.getController(), "get"));
+				.isEqualTo(new HandlerMethod(source, ReflectionUtils.findMethod(source.getClass(), "get")));
+		Object source1 = second.getController();
 		assertThat(mapping.getHandler(request("POST", "/second")).getHandler())
-				.isEqualTo(handlerOf(second.getController(), "save"));
+				.isEqualTo(new HandlerMethod(source1, ReflectionUtils.findMethod(source1.getClass(), "save")));
 		assertThat(mapping.getHandler(request("GET", "/third"))).isNull();
 	}
 
@@ -64,10 +66,12 @@ class ControllerEndpointHandlerMappingTests {
 		ExposableControllerEndpoint first = firstEndpoint();
 		ExposableControllerEndpoint second = secondEndpoint();
 		ControllerEndpointHandlerMapping mapping = createMapping("actuator", first, second);
+		Object source = first.getController();
 		assertThat(mapping.getHandler(request("GET", "/actuator/first")).getHandler())
-				.isEqualTo(handlerOf(first.getController(), "get"));
+				.isEqualTo(new HandlerMethod(source, ReflectionUtils.findMethod(source.getClass(), "get")));
+		Object source1 = second.getController();
 		assertThat(mapping.getHandler(request("POST", "/actuator/second")).getHandler())
-				.isEqualTo(handlerOf(second.getController(), "save"));
+				.isEqualTo(new HandlerMethod(source1, ReflectionUtils.findMethod(source1.getClass(), "save")));
 		assertThat(mapping.getHandler(request("GET", "/first"))).isNull();
 		assertThat(mapping.getHandler(request("GET", "/second"))).isNull();
 	}
@@ -84,8 +88,9 @@ class ControllerEndpointHandlerMappingTests {
 	void mappingWithNoPath() throws Exception {
 		ExposableControllerEndpoint pathless = pathlessEndpoint();
 		ControllerEndpointHandlerMapping mapping = createMapping("actuator", pathless);
+		Object source = pathless.getController();
 		assertThat(mapping.getHandler(request("GET", "/actuator/pathless")).getHandler())
-				.isEqualTo(handlerOf(pathless.getController(), "get"));
+				.isEqualTo(new HandlerMethod(source, ReflectionUtils.findMethod(source.getClass(), "get")));
 		assertThat(mapping.getHandler(request("GET", "/pathless"))).isNull();
 		assertThat(mapping.getHandler(request("GET", "/"))).isNull();
 	}
@@ -96,10 +101,6 @@ class ControllerEndpointHandlerMappingTests {
 		mapping.setApplicationContext(this.context);
 		mapping.afterPropertiesSet();
 		return mapping;
-	}
-
-	private HandlerMethod handlerOf(Object source, String methodName) {
-		return new HandlerMethod(source, ReflectionUtils.findMethod(source.getClass(), methodName));
 	}
 
 	private MockHttpServletRequest request(String method, String requestURI) {
