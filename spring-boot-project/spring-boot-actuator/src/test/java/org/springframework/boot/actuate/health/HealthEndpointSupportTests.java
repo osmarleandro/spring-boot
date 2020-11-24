4300314a00588726749c3a0c224d72eaa07d7f40
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 07:26:05 -0300

    spring-boot-actuator,org.springframework.boot.actuate.health,HealthEndpointSupportTests,getHealthWhenAlwaysShowIsFalseAndGroupIsFalseCannotAccessComponent#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/HealthEndpointSupportTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/HealthEndpointSupportTests.java
@@ -110,19 +110,6 @@ abstract class HealthEndpointSupportTests<R extends ContributorRegistry<C>, C, T
 	}
 
 	@Test
-	void getHealthWhenAlwaysShowIsFalseAndGroupIsFalseCannotAccessComponent() {
-		this.primaryGroup.setShowComponents(false);
-		C contributor = createContributor(this.up);
-		C compositeContributor = createCompositeContributor(Collections.singletonMap("spring", contributor));
-		this.registry.registerContributor("test", compositeContributor);
-		HealthEndpointSupport<C, T> endpoint = create(this.registry, this.groups);
-		HealthResult<T> rootResult = endpoint.getHealth(ApiVersion.V3, SecurityContext.NONE, false);
-		assertThat(((CompositeHealth) getHealth(rootResult)).getComponents()).isNullOrEmpty();
-		HealthResult<T> componentResult = endpoint.getHealth(ApiVersion.V3, SecurityContext.NONE, false, "test");
-		assertThat(componentResult).isNull();
-	}
-
-	@Test
 	void getHealthWhenAlwaysShowIsTrueShowsComponents() {
 		this.primaryGroup.setShowComponents(true);
 		C contributor = createContributor(this.up);
