ed696d23d9aeae28a8ae4690ffcf06bc0dd80ff5
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 15:57:26 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.couchbase,CouchbaseHealthIndicatorTests,couchbaseClusterIsDown#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/couchbase/CouchbaseHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/couchbase/CouchbaseHealthIndicatorTests.java
@@ -64,25 +64,4 @@ class CouchbaseHealthIndicatorTests {
 		verify(cluster).diagnostics();
 	}
 
-	@Test
-	@SuppressWarnings("unchecked")
-	void couchbaseClusterIsDown() {
-		Cluster cluster = mock(Cluster.class);
-		CouchbaseHealthIndicator healthIndicator = new CouchbaseHealthIndicator(cluster);
-		Map<ServiceType, List<EndpointDiagnostics>> endpoints = Collections.singletonMap(ServiceType.KV,
-				Arrays.asList(
-						new EndpointDiagnostics(ServiceType.KV, EndpointState.CONNECTED, "127.0.0.1", "127.0.0.1",
-								Optional.empty(), Optional.of(1234L), Optional.of("endpoint-1")),
-						new EndpointDiagnostics(ServiceType.KV, EndpointState.CONNECTING, "127.0.0.1", "127.0.0.1",
-								Optional.empty(), Optional.of(1234L), Optional.of("endpoint-2"))));
-		DiagnosticsResult diagnostics = new DiagnosticsResult(endpoints, "test-sdk", "test-id");
-		given(cluster.diagnostics()).willReturn(diagnostics);
-		Health health = healthIndicator.health();
-		assertThat(health.getStatus()).isEqualTo(Status.DOWN);
-		assertThat(health.getDetails()).containsEntry("sdk", "test-sdk");
-		assertThat(health.getDetails()).containsKey("endpoints");
-		assertThat((List<Map<String, Object>>) health.getDetails().get("endpoints")).hasSize(2);
-		verify(cluster).diagnostics();
-	}
-
 }
