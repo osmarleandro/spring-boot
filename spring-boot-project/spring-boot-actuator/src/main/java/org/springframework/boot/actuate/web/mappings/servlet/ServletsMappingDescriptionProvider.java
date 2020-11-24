b71c6e7cd59490781e94257d9e7fef1881639604
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 14:43:23 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.web.mappings.servlet,ServletsMappingDescriptionProvider,describeMappings#QApplicationContext,InlineMethod,BUILDFAILED,false,true,1

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/mappings/servlet/ServletsMappingDescriptionProvider.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/web/mappings/servlet/ServletsMappingDescriptionProvider.java
@@ -37,15 +37,6 @@ import org.springframework.web.context.WebApplicationContext;
 public class ServletsMappingDescriptionProvider implements MappingDescriptionProvider {
 
 	@Override
-	public List<ServletRegistrationMappingDescription> describeMappings(ApplicationContext context) {
-		if (!(context instanceof WebApplicationContext)) {
-			return Collections.emptyList();
-		}
-		return ((WebApplicationContext) context).getServletContext().getServletRegistrations().values().stream()
-				.map(ServletRegistrationMappingDescription::new).collect(Collectors.toList());
-	}
-
-	@Override
 	public String getMappingName() {
 		return "servlets";
 	}
