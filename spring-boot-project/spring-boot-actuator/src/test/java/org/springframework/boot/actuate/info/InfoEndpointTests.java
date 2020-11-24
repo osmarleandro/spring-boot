0d7268d88a2a1a8ec65770f0b1a375873d5b7b31
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 07:35:26 -0300

    spring-boot-actuator,org.springframework.boot.actuate.info,InfoEndpointTests,info#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/info/InfoEndpointTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/info/InfoEndpointTests.java
@@ -35,16 +35,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 class InfoEndpointTests {
 
 	@Test
-	void info() {
-		InfoEndpoint endpoint = new InfoEndpoint(Arrays.asList((builder) -> builder.withDetail("key1", "value1"),
-				(builder) -> builder.withDetail("key2", "value2")));
-		Map<String, Object> info = endpoint.info();
-		assertThat(info).hasSize(2);
-		assertThat(info).containsEntry("key1", "value1");
-		assertThat(info).containsEntry("key2", "value2");
-	}
-
-	@Test
 	void infoWithNoContributorsProducesEmptyMap() {
 		InfoEndpoint endpoint = new InfoEndpoint(Collections.emptyList());
 		Map<String, Object> info = endpoint.info();
