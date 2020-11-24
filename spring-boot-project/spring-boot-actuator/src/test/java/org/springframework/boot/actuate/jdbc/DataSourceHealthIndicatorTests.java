182c28c3db744dd9eaefb0ebd729b55e8c1ee9af
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 07:41:37 -0300

    spring-boot-actuator,org.springframework.boot.actuate.jdbc,DataSourceHealthIndicatorTests,healthIndicatorWithConnectionValidationFailure#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/jdbc/DataSourceHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/jdbc/DataSourceHealthIndicatorTests.java
@@ -109,18 +109,4 @@ class DataSourceHealthIndicatorTests {
 		verify(connection, times(2)).close();
 	}
 
-	@Test
-	void healthIndicatorWithConnectionValidationFailure() throws SQLException {
-		DataSource dataSource = mock(DataSource.class);
-		Connection connection = mock(Connection.class);
-		given(connection.isValid(0)).willReturn(false);
-		given(connection.getMetaData()).willReturn(this.dataSource.getConnection().getMetaData());
-		given(dataSource.getConnection()).willReturn(connection);
-		this.indicator.setDataSource(dataSource);
-		Health health = this.indicator.health();
-		assertThat(health.getStatus()).isEqualTo(Status.DOWN);
-		assertThat(health.getDetails()).containsOnly(entry("database", "HSQL Database Engine"),
-				entry("validationQuery", "isValid()"));
-	}
-
 }
