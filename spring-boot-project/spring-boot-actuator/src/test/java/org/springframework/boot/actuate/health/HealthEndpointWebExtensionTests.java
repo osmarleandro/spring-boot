6e8330205839f3daacbd60a37480f99784f1d2b8
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 18:42:05 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.health,HealthEndpointWebExtensionTests,healthReturnsSystemHealth#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/HealthEndpointWebExtensionTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/health/HealthEndpointWebExtensionTests.java
@@ -39,17 +39,6 @@ class HealthEndpointWebExtensionTests
 		extends HealthEndpointSupportTests<HealthContributorRegistry, HealthContributor, HealthComponent> {
 
 	@Test
-	void healthReturnsSystemHealth() {
-		this.registry.registerContributor("test", createContributor(this.up));
-		WebEndpointResponse<HealthComponent> response = create(this.registry, this.groups).health(ApiVersion.LATEST,
-				SecurityContext.NONE);
-		HealthComponent health = response.getBody();
-		assertThat(health.getStatus()).isEqualTo(Status.UP);
-		assertThat(health).isInstanceOf(SystemHealth.class);
-		assertThat(response.getStatus()).isEqualTo(200);
-	}
-
-	@Test
 	void healthWithNoContributorReturnsUp() {
 		assertThat(this.registry).isEmpty();
 		WebEndpointResponse<HealthComponent> response = create(this.registry,
