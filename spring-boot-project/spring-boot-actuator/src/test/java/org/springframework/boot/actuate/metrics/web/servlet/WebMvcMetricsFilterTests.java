0a0661bcfcdf24fa0ce740bbcb6899f77eb8ae6c
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 20:36:53 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.metrics.web.servlet,WebMvcMetricsFilterTests,endpointThrowsError#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/WebMvcMetricsFilterTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/servlet/WebMvcMetricsFilterTests.java
@@ -262,12 +262,6 @@ class WebMvcMetricsFilterTests {
 	}
 
 	@Test
-	void endpointThrowsError() throws Exception {
-		this.mvc.perform(get("/api/c1/error/10")).andExpect(status().is4xxClientError());
-		assertThat(this.registry.get("http.server.requests").tags("status", "422").timer().count()).isEqualTo(1L);
-	}
-
-	@Test
 	void regexBasedRequestMapping() throws Exception {
 		this.mvc.perform(get("/api/c1/regex/.abc")).andExpect(status().isOk());
 		assertThat(
