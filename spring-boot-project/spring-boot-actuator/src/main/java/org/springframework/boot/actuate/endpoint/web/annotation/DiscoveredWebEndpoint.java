f44b6fb28e77fc551de5007616e626aa25789893
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 02:34:46 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.web.annotation,DiscoveredWebEndpoint,getRootPath#,InlineMethod,BUILDFAILED,false,true,18

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/annotation/DiscoveredWebEndpoint.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/annotation/DiscoveredWebEndpoint.java
@@ -39,9 +39,4 @@ class DiscoveredWebEndpoint extends AbstractDiscoveredEndpoint<WebOperation> imp
 		this.rootPath = rootPath;
 	}
 
-	@Override
-	public String getRootPath() {
-		return this.rootPath;
-	}
-
 }
