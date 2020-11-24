1d12d1c05281403a101039e5340e37dce1bd7efd
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 07:42:04 -0300

    spring-boot-actuator,org.springframework.boot.actuate.jms,JmsHealthIndicatorTests,jmsBrokerIsDown#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/jms/JmsHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/jms/JmsHealthIndicatorTests.java
@@ -59,16 +59,6 @@ class JmsHealthIndicatorTests {
 	}
 
 	@Test
-	void jmsBrokerIsDown() throws JMSException {
-		ConnectionFactory connectionFactory = mock(ConnectionFactory.class);
-		given(connectionFactory.createConnection()).willThrow(new JMSException("test", "123"));
-		JmsHealthIndicator indicator = new JmsHealthIndicator(connectionFactory);
-		Health health = indicator.health();
-		assertThat(health.getStatus()).isEqualTo(Status.DOWN);
-		assertThat(health.getDetails().get("provider")).isNull();
-	}
-
-	@Test
 	void jmsBrokerCouldNotRetrieveProviderMetadata() throws JMSException {
 		ConnectionMetaData connectionMetaData = mock(ConnectionMetaData.class);
 		given(connectionMetaData.getJMSProviderName()).willThrow(new JMSException("test", "123"));
