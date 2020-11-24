a8497d460ba3e91464873598cc35137bb94ab064
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 20:22:14 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.metrics.web.reactive.client,WebClientExchangeTagsTests,outcomeTagIsClientErrorWhenResponseIsNonStandardInClientSeries#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/reactive/client/WebClientExchangeTagsTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/metrics/web/reactive/client/WebClientExchangeTagsTests.java
@@ -161,13 +161,6 @@ class WebClientExchangeTagsTests {
 	}
 
 	@Test
-	void outcomeTagIsClientErrorWhenResponseIsNonStandardInClientSeries() {
-		given(this.response.rawStatusCode()).willReturn(490);
-		Tag tag = WebClientExchangeTags.outcome(this.response);
-		assertThat(tag.getValue()).isEqualTo("CLIENT_ERROR");
-	}
-
-	@Test
 	void outcomeTagIsUnknownWhenResponseStatusIsInUnknownSeries() {
 		given(this.response.rawStatusCode()).willReturn(701);
 		Tag tag = WebClientExchangeTags.outcome(this.response);
