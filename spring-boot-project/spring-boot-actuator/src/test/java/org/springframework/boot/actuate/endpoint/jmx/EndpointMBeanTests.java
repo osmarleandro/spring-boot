aeb6dcefe02af0909d0e966eb1f912cfd2b7389f
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 16:44:40 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.endpoint.jmx,EndpointMBeanTests,invokeShouldInvokeJmxOperationWithBeanClassLoader#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/jmx/EndpointMBeanTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/jmx/EndpointMBeanTests.java
@@ -119,18 +119,6 @@ class EndpointMBeanTests {
 	}
 
 	@Test
-	void invokeShouldInvokeJmxOperationWithBeanClassLoader() throws ReflectionException, MBeanException {
-		ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
-		TestExposableJmxEndpoint endpoint = new TestExposableJmxEndpoint(
-				new TestJmxOperation((arguments) -> ClassUtils.getDefaultClassLoader()));
-		URLClassLoader beanClassLoader = new URLClassLoader(new URL[0], getClass().getClassLoader());
-		EndpointMBean bean = new EndpointMBean(this.responseMapper, beanClassLoader, endpoint);
-		Object result = bean.invoke("testOperation", NO_PARAMS, NO_SIGNATURE);
-		assertThat(result).isEqualTo(beanClassLoader);
-		assertThat(Thread.currentThread().getContextClassLoader()).isEqualTo(originalClassLoader);
-	}
-
-	@Test
 	void invokeWhenOperationIsInvalidShouldThrowException() throws MBeanException, ReflectionException {
 		TestJmxOperation operation = new TestJmxOperation() {
 
