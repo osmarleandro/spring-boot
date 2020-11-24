ac4dbcfd0679b536f38a7ad9c11c1394acba280f
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 17:46:38 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.endpoint.web.annotation,AbstractWebEndpointIntegrationTests,readOperationWithListQueryParameterAndSingleValue#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/annotation/AbstractWebEndpointIntegrationTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/annotation/AbstractWebEndpointIntegrationTests.java
@@ -154,12 +154,6 @@ public abstract class AbstractWebEndpointIntegrationTests<T extends Configurable
 	}
 
 	@Test
-	void readOperationWithListQueryParameterAndSingleValue() {
-		load(QueryWithListEndpointConfiguration.class, (client) -> client.get().uri("/query?one=1&two=2").exchange()
-				.expectStatus().isOk().expectBody().jsonPath("query").isEqualTo("1 [2]"));
-	}
-
-	@Test
 	void readOperationWithListQueryParameterAndMultipleValues() {
 		load(QueryWithListEndpointConfiguration.class, (client) -> client.get().uri("/query?one=1&two=2&two=2")
 				.exchange().expectStatus().isOk().expectBody().jsonPath("query").isEqualTo("1 [2, 2]"));
