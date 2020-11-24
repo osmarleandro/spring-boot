0c8f43cabd8d992559b9b5cf31f19db15fef91b1
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 02:04:03 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.jmx.annotation,JmxEndpointDiscoverer,createOperation#QEndpointId;QDiscoveredOperationMethod;QOperationInvoker,InlineMethod,BUILDFAILED,false,true,1

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/jmx/annotation/JmxEndpointDiscoverer.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/jmx/annotation/JmxEndpointDiscoverer.java
@@ -59,12 +59,6 @@ public class JmxEndpointDiscoverer extends EndpointDiscoverer<ExposableJmxEndpoi
 	}
 
 	@Override
-	protected JmxOperation createOperation(EndpointId endpointId, DiscoveredOperationMethod operationMethod,
-			OperationInvoker invoker) {
-		return new DiscoveredJmxOperation(endpointId, operationMethod, invoker);
-	}
-
-	@Override
 	protected OperationKey createOperationKey(JmxOperation operation) {
 		return new OperationKey(operation.getName(), () -> "MBean call '" + operation.getName() + "'");
 	}
