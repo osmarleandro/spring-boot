1ef0d4ad588cfe24b0c5b0b890eba8b2419c5025
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-24 17:45:55 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.endpoint.web.annotation,AbstractWebEndpointIntegrationTests,readOperationWithSingleQueryParameters#,InlineMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/annotation/AbstractWebEndpointIntegrationTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/annotation/AbstractWebEndpointIntegrationTests.java
@@ -142,12 +142,6 @@ public abstract class AbstractWebEndpointIntegrationTests<T extends Configurable
 	}
 
 	@Test
-	void readOperationWithSingleQueryParameters() {
-		load(QueryEndpointConfiguration.class, (client) -> client.get().uri("/query?one=1&two=2").exchange()
-				.expectStatus().isOk().expectBody().jsonPath("query").isEqualTo("1 2"));
-	}
-
-	@Test
 	void readOperationWithSingleQueryParametersAndMultipleValues() {
 		load(QueryEndpointConfiguration.class, (client) -> client.get().uri("/query?one=1&one=1&two=2").exchange()
 				.expectStatus().isOk().expectBody().jsonPath("query").isEqualTo("1,1 2"));
