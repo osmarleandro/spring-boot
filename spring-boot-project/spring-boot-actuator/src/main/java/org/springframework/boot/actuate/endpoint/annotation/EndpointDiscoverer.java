680e2cdbdb28d2b410b3d3a3127e884b4e3b61a3
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-26 04:47:18 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.annotation,EndpointDiscoverer,createEndpoint#QObject;QEndpointId;Z;QCollectionQO,RenameMethod,BUILDFAILED

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/annotation/EndpointDiscoverer.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/annotation/EndpointDiscoverer.java
@@ -201,7 +201,7 @@ public abstract class EndpointDiscoverer<E extends ExposableEndpoint<O>, O exten
 		assertNoDuplicateOperations(endpointBean, indexed);
 		List<O> operations = indexed.values().stream().map(this::getLast).filter(Objects::nonNull)
 				.collect(Collectors.collectingAndThen(Collectors.toList(), Collections::unmodifiableList));
-		return createEndpoint(endpointBean.getBean(), id, endpointBean.isEnabledByDefault(), operations);
+		return createEndpoint_RENAMED(endpointBean.getBean(), id, endpointBean.isEnabledByDefault(), operations);
 	}
 
 	private void addOperations(MultiValueMap<OperationKey, O> indexed, EndpointId id, Object target,
@@ -326,7 +326,7 @@ public abstract class EndpointDiscoverer<E extends ExposableEndpoint<O>, O exten
 	private E getFilterEndpoint(EndpointBean endpointBean) {
 		E endpoint = this.filterEndpoints.get(endpointBean);
 		if (endpoint == null) {
-			endpoint = createEndpoint(endpointBean.getBean(), endpointBean.getId(), endpointBean.isEnabledByDefault(),
+			endpoint = createEndpoint_RENAMED(endpointBean.getBean(), endpointBean.getId(), endpointBean.isEnabledByDefault(),
 					Collections.emptySet());
 			this.filterEndpoints.put(endpointBean, endpoint);
 		}
@@ -346,7 +346,7 @@ public abstract class EndpointDiscoverer<E extends ExposableEndpoint<O>, O exten
 	 * @param operations the endpoint operations
 	 * @return a created endpoint (a {@link DiscoveredEndpoint} is recommended)
 	 */
-	protected abstract E createEndpoint(Object endpointBean, EndpointId id, boolean enabledByDefault,
+	protected abstract E createEndpoint_RENAMED(Object endpointBean, EndpointId id, boolean enabledByDefault,
 			Collection<O> operations);
 
 	/**
