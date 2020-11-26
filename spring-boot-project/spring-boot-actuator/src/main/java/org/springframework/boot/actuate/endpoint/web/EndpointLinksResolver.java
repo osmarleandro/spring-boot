f3e4e85ecd761057da415331593409571fded8d5
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-26 09:12:01 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.web,EndpointLinksResolver,createLink#QString;QString,RenameMethod,OK

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/EndpointLinksResolver.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/EndpointLinksResolver.java
@@ -75,7 +75,7 @@ public class EndpointLinksResolver {
 			}
 			else if (endpoint instanceof PathMappedEndpoint) {
 				String rootPath = ((PathMappedEndpoint) endpoint).getRootPath();
-				Link link = createLink(normalizedUrl, rootPath);
+				Link link = createLink_RENAMED(normalizedUrl, rootPath);
 				links.put(endpoint.getEndpointId().toLowerCaseString(), link);
 			}
 		}
@@ -96,10 +96,10 @@ public class EndpointLinksResolver {
 	}
 
 	private Link createLink(String requestUrl, WebOperation operation) {
-		return createLink(requestUrl, operation.getRequestPredicate().getPath());
+		return createLink_RENAMED(requestUrl, operation.getRequestPredicate().getPath());
 	}
 
-	private Link createLink(String requestUrl, String path) {
+	private Link createLink_RENAMED(String requestUrl, String path) {
 		return new Link(requestUrl + (path.startsWith("/") ? path : "/" + path));
 	}
 
