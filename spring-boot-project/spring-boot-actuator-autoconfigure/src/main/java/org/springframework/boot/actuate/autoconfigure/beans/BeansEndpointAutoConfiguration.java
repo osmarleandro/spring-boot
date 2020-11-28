bb46673b285cb32becc83bec38920710a0ec63e8
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-28 02:10:44 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.beans,BeansEndpointAutoConfiguration,beansEndpoint#QConfigurableApplicationContext,RenameMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/beans/BeansEndpointAutoConfiguration.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/beans/BeansEndpointAutoConfiguration.java
@@ -36,7 +36,7 @@ public class BeansEndpointAutoConfiguration {
 
 	@Bean
 	@ConditionalOnMissingBean
-	public BeansEndpoint beansEndpoint(ConfigurableApplicationContext applicationContext) {
+	public BeansEndpoint beansEndpoint_RENAMED(ConfigurableApplicationContext applicationContext) {
 		return new BeansEndpoint(applicationContext);
 	}
 
