aca4751f87c831076fb7798eafe3fc345426f68e
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 02:37:52 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.flyway,FlywayEndpointAutoConfigurationTests,runWhenNotExposedShouldNotHaveEndpointBean#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/flyway/FlywayEndpointAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/flyway/FlywayEndpointAutoConfigurationTests.java
@@ -31,9 +31,9 @@ import static org.mockito.Mockito.mock;
  *
  * @author Phillip Webb
  */
-class FlywayEndpointAutoConfigurationTests {
+public class FlywayEndpointAutoConfigurationTests {
 
-	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+	public final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(FlywayEndpointAutoConfiguration.class))
 			.withBean(Flyway.class, () -> mock(Flyway.class));
 
@@ -47,11 +47,6 @@ class FlywayEndpointAutoConfigurationTests {
 	void runWhenEnabledPropertyIsFalseShouldNotHaveEndpointBean() {
 		this.contextRunner.withPropertyValues("management.endpoint.flyway.enabled:false")
 				.run((context) -> assertThat(context).doesNotHaveBean(FlywayEndpoint.class));
-	}
-
-	@Test
-	void runWhenNotExposedShouldNotHaveEndpointBean() {
-		this.contextRunner.run((context) -> assertThat(context).doesNotHaveBean(FlywayEndpoint.class));
 	}
 
 }
