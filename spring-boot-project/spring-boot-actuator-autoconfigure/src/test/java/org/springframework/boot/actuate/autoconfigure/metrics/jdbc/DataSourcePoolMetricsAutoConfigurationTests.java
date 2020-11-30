a8d76615eb0658daa0d7ee437a29acaa5a85046b
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 08:44:34 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.metrics.jdbc,DataSourcePoolMetricsAutoConfigurationTests,dataSourceInstrumentationCanBeDisabled#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/jdbc/DataSourcePoolMetricsAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/jdbc/DataSourcePoolMetricsAutoConfigurationTests.java
@@ -51,9 +51,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Andy Wilkinson
  * @author Tommy Ludwig
  */
-class DataSourcePoolMetricsAutoConfigurationTests {
+public class DataSourcePoolMetricsAutoConfigurationTests {
 
-	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+	public final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withPropertyValues("spring.datasource.generate-unique-name=true").with(MetricsRun.simple())
 			.withConfiguration(AutoConfigurations.of(DataSourcePoolMetricsAutoConfiguration.class))
 			.withUserConfiguration(BaseConfiguration.class);
@@ -65,16 +65,6 @@ class DataSourcePoolMetricsAutoConfigurationTests {
 					context.getBean(DataSource.class).getConnection().getMetaData();
 					MeterRegistry registry = context.getBean(MeterRegistry.class);
 					registry.get("jdbc.connections.max").tags("name", "dataSource").meter();
-				});
-	}
-
-	@Test
-	void dataSourceInstrumentationCanBeDisabled() {
-		this.contextRunner.withConfiguration(AutoConfigurations.of(DataSourceAutoConfiguration.class))
-				.withPropertyValues("management.metrics.enable.jdbc=false").run((context) -> {
-					context.getBean(DataSource.class).getConnection().getMetaData();
-					MeterRegistry registry = context.getBean(MeterRegistry.class);
-					assertThat(registry.find("jdbc.connections.max").tags("name", "dataSource").meter()).isNull();
 				});
 	}
 
