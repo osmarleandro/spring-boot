c480fc6c93b7959b06e52f020860a2adb3111350
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 06:04:39 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.metrics,MetricsAutoConfigurationWithLog4j2AndLogbackTests,doesNotConfigureLogbackMetrics#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/MetricsAutoConfigurationWithLog4j2AndLogbackTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/MetricsAutoConfigurationWithLog4j2AndLogbackTests.java
@@ -16,14 +16,9 @@
 
 package org.springframework.boot.actuate.autoconfigure.metrics;
 
-import io.micrometer.core.instrument.binder.logging.LogbackMetrics;
-import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.boot.testsupport.classpath.ClassPathOverrides;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Tests for {@link MetricsAutoConfiguration} when both Log4j2 and Logback are on the
@@ -32,14 +27,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Andy Wilkinson
  */
 @ClassPathOverrides({ "org.apache.logging.log4j:log4j-core:2.9.0", "org.apache.logging.log4j:log4j-slf4j-impl:2.9.0" })
+public
 class MetricsAutoConfigurationWithLog4j2AndLogbackTests {
 
-	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+	public final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(MetricsAutoConfiguration.class));
-
-	@Test
-	void doesNotConfigureLogbackMetrics() {
-		this.contextRunner.run((context) -> assertThat(context).doesNotHaveBean(LogbackMetrics.class));
-	}
 
 }
