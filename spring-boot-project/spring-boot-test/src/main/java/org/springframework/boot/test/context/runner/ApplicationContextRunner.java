63d25b9ed3e5264a3b8fe25203116ef59ec782a9
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-29 21:15:32 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.cassandra,CassandraHealthContributorAutoConfigurationTests,runWhenDisabledShouldNotCreateIndicator#,MoveMethod,OK

--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/ApplicationContextRunner.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/ApplicationContextRunner.java
@@ -16,9 +16,15 @@
 
 package org.springframework.boot.test.context.runner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
 import java.util.List;
 import java.util.function.Supplier;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.autoconfigure.cassandra.CassandraHealthContributorAutoConfigurationTests;
+import org.springframework.boot.actuate.cassandra.CassandraDriverHealthIndicator;
 import org.springframework.boot.context.annotation.Configurations;
 import org.springframework.boot.test.context.assertj.AssertableApplicationContext;
 import org.springframework.boot.test.util.TestPropertyValues;
@@ -26,6 +32,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.cassandra.core.CassandraOperations;
+
+import com.datastax.oss.driver.api.core.CqlSession;
 
 /**
  * An {@link AbstractApplicationContextRunner ApplicationContext runner} for a standard,
@@ -77,6 +86,17 @@ public class ApplicationContextRunner extends
 			List<Configurations> configurations) {
 		return new ApplicationContextRunner(contextFactory, allowBeanDefinitionOverriding, initializers,
 				environmentProperties, systemProperties, classLoader, parent, beanRegistrations, configurations);
+	}
+
+	@Test
+	public
+	void runWhenDisabledShouldNotCreateIndicator(CassandraHealthContributorAutoConfigurationTests cassandraHealthContributorAutoConfigurationTests) {
+		withBean(CqlSession.class, () -> mock(CqlSession.class))
+				.withBean(CassandraOperations.class, () -> mock(CassandraOperations.class))
+				.withPropertyValues("management.health.cassandra.enabled:false")
+				.run((context) -> assertThat(context).doesNotHaveBean("cassandraHealthContributor")
+						.doesNotHaveBean(org.springframework.boot.actuate.cassandra.CassandraHealthIndicator.class)
+						.doesNotHaveBean(CassandraDriverHealthIndicator.class));
 	}
 
 }
