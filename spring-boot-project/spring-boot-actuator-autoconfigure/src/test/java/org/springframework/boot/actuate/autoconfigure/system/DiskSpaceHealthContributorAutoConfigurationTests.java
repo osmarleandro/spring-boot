5efed5a36a4c7121a08052d837c7b57e2daaf1ae
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 11:08:47 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.system,DiskSpaceHealthContributorAutoConfigurationTests,thresholdMustBePositive#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/system/DiskSpaceHealthContributorAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/system/DiskSpaceHealthContributorAutoConfigurationTests.java
@@ -32,22 +32,15 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Phillip Webb
  * @author Stephane Nicoll
  */
-class DiskSpaceHealthContributorAutoConfigurationTests {
+public class DiskSpaceHealthContributorAutoConfigurationTests {
 
-	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+	public final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(DiskSpaceHealthContributorAutoConfiguration.class,
 					HealthContributorAutoConfiguration.class));
 
 	@Test
 	void runShouldCreateIndicator() {
 		this.contextRunner.run((context) -> assertThat(context).hasSingleBean(DiskSpaceHealthIndicator.class));
-	}
-
-	@Test
-	void thresholdMustBePositive() {
-		this.contextRunner.withPropertyValues("management.health.diskspace.threshold=-10MB")
-				.run((context) -> assertThat(context).hasFailed().getFailure()
-						.hasMessageContaining("Failed to bind properties under 'management.health.diskspace'"));
 	}
 
 	@Test
