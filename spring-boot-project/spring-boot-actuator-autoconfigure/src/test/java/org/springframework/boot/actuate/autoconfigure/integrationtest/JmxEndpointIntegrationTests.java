10415e70f8c642364729703ccea04f1d6d9ebecd
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 04:28:10 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.integrationtest,JmxEndpointIntegrationTests,checkEndpointMBeans#QMBeanServer;QString;QString,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/integrationtest/JmxEndpointIntegrationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/integrationtest/JmxEndpointIntegrationTests.java
@@ -39,15 +39,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 /**
  * Integration tests for endpoints over JMX.
  *
  * @author Stephane Nicoll
  * @author Andy Wilkinson
  */
-class JmxEndpointIntegrationTests {
+public class JmxEndpointIntegrationTests {
 
 	private final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(JmxAutoConfiguration.class, EndpointAutoConfiguration.class,
@@ -61,7 +59,7 @@ class JmxEndpointIntegrationTests {
 	void jmxEndpointsAreExposed() {
 		this.contextRunner.run((context) -> {
 			MBeanServer mBeanServer = context.getBean(MBeanServer.class);
-			checkEndpointMBeans(mBeanServer, new String[] { "beans", "conditions", "configprops", "env", "health",
+			contextRunner.checkEndpointMBeans(this, mBeanServer, new String[] { "beans", "conditions", "configprops", "env", "health",
 					"info", "mappings", "threaddump", "httptrace" }, new String[] { "shutdown" });
 		});
 	}
@@ -70,7 +68,7 @@ class JmxEndpointIntegrationTests {
 	void jmxEndpointsCanBeExcluded() {
 		this.contextRunner.withPropertyValues("management.endpoints.jmx.exposure.exclude:*").run((context) -> {
 			MBeanServer mBeanServer = context.getBean(MBeanServer.class);
-			checkEndpointMBeans(mBeanServer, new String[0], new String[] { "beans", "conditions", "configprops", "env",
+			contextRunner.checkEndpointMBeans(this, mBeanServer, new String[0], new String[] { "beans", "conditions", "configprops", "env",
 					"health", "mappings", "shutdown", "threaddump", "httptrace" });
 
 		});
@@ -80,23 +78,12 @@ class JmxEndpointIntegrationTests {
 	void singleJmxEndpointCanBeExposed() {
 		this.contextRunner.withPropertyValues("management.endpoints.jmx.exposure.include=beans").run((context) -> {
 			MBeanServer mBeanServer = context.getBean(MBeanServer.class);
-			checkEndpointMBeans(mBeanServer, new String[] { "beans" }, new String[] { "conditions", "configprops",
+			contextRunner.checkEndpointMBeans(this, mBeanServer, new String[] { "beans" }, new String[] { "conditions", "configprops",
 					"env", "health", "mappings", "shutdown", "threaddump", "httptrace" });
 		});
 	}
 
-	private void checkEndpointMBeans(MBeanServer mBeanServer, String[] enabledEndpoints, String[] disabledEndpoints) {
-		for (String enabledEndpoint : enabledEndpoints) {
-			assertThat(isRegistered(mBeanServer, getDefaultObjectName(enabledEndpoint)))
-					.as(String.format("Endpoint %s", enabledEndpoint)).isTrue();
-		}
-		for (String disabledEndpoint : disabledEndpoints) {
-			assertThat(isRegistered(mBeanServer, getDefaultObjectName(disabledEndpoint)))
-					.as(String.format("Endpoint %s", disabledEndpoint)).isFalse();
-		}
-	}
-
-	private boolean isRegistered(MBeanServer mBeanServer, ObjectName objectName) {
+	public boolean isRegistered(MBeanServer mBeanServer, ObjectName objectName) {
 		try {
 			getMBeanInfo(mBeanServer, objectName);
 			return true;
@@ -115,7 +102,7 @@ class JmxEndpointIntegrationTests {
 		}
 	}
 
-	private ObjectName getDefaultObjectName(String endpointId) {
+	public ObjectName getDefaultObjectName(String endpointId) {
 		return getObjectName("org.springframework.boot", endpointId);
 	}
 
--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/WebApplicationContextRunner.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/WebApplicationContextRunner.java
@@ -16,9 +16,14 @@
 
 package org.springframework.boot.test.context.runner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.management.MBeanServer;
+
+import org.springframework.boot.actuate.autoconfigure.integrationtest.JmxEndpointIntegrationTests;
 import org.springframework.boot.context.annotation.Configurations;
 import org.springframework.boot.test.context.assertj.AssertableWebApplicationContext;
 import org.springframework.boot.test.util.TestPropertyValues;
@@ -81,6 +86,17 @@ public final class WebApplicationContextRunner extends
 			List<Configurations> configurations) {
 		return new WebApplicationContextRunner(contextFactory, allowBeanDefinitionOverriding, initializers,
 				environmentProperties, systemProperties, classLoader, parent, beanRegistrations, configurations);
+	}
+
+	public void checkEndpointMBeans(JmxEndpointIntegrationTests jmxEndpointIntegrationTests, MBeanServer mBeanServer, String[] enabledEndpoints, String[] disabledEndpoints) {
+		for (String enabledEndpoint : enabledEndpoints) {
+			assertThat(jmxEndpointIntegrationTests.isRegistered(mBeanServer, jmxEndpointIntegrationTests.getDefaultObjectName(enabledEndpoint)))
+					.as(String.format("Endpoint %s", enabledEndpoint)).isTrue();
+		}
+		for (String disabledEndpoint : disabledEndpoints) {
+			assertThat(jmxEndpointIntegrationTests.isRegistered(mBeanServer, jmxEndpointIntegrationTests.getDefaultObjectName(disabledEndpoint)))
+					.as(String.format("Endpoint %s", disabledEndpoint)).isFalse();
+		}
 	}
 
 	/**
