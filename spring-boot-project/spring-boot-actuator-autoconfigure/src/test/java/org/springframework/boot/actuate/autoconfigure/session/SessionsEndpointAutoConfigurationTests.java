76ba1e1fa1be0e1e83ef1449a2b1124e1b9b449b
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 10:59:52 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.session,SessionsEndpointAutoConfigurationTests,runShouldHaveEndpointBean#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/session/SessionsEndpointAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/session/SessionsEndpointAutoConfigurationTests.java
@@ -33,17 +33,11 @@ import static org.mockito.Mockito.mock;
  *
  * @author Vedran Pavic
  */
-class SessionsEndpointAutoConfigurationTests {
+public class SessionsEndpointAutoConfigurationTests {
 
-	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+	public final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(SessionsEndpointAutoConfiguration.class))
 			.withUserConfiguration(SessionConfiguration.class);
-
-	@Test
-	void runShouldHaveEndpointBean() {
-		this.contextRunner.withPropertyValues("management.endpoints.web.exposure.include=sessions")
-				.run((context) -> assertThat(context).hasSingleBean(SessionsEndpoint.class));
-	}
 
 	@Test
 	void runWhenNotExposedShouldNotHaveEndpointBean() {
