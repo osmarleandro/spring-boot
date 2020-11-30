079291c0115122444dea74dac68e466325ee82af
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 06:59:40 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.metrics.export.ganglia,GangliaMetricsExportAutoConfigurationTests,backsOffWithoutAClock#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/ganglia/GangliaMetricsExportAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/ganglia/GangliaMetricsExportAutoConfigurationTests.java
@@ -34,15 +34,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
-class GangliaMetricsExportAutoConfigurationTests {
+public class GangliaMetricsExportAutoConfigurationTests {
 
-	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+	public final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(GangliaMetricsExportAutoConfiguration.class));
-
-	@Test
-	void backsOffWithoutAClock() {
-		this.contextRunner.run((context) -> assertThat(context).doesNotHaveBean(GangliaMeterRegistry.class));
-	}
 
 	@Test
 	void autoConfiguresItsConfigAndMeterRegistry() {
