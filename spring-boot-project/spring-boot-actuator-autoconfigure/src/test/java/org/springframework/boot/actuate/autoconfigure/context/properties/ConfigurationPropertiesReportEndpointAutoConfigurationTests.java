2c24c72e225a29f6fb5ab901f9bb6983b50ad6e6
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 00:29:09 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.context.properties,ConfigurationPropertiesReportEndpointAutoConfigurationTests,runWhenEnabledPropertyIsFalseShouldNotHaveEndpointBean#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/context/properties/ConfigurationPropertiesReportEndpointAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/context/properties/ConfigurationPropertiesReportEndpointAutoConfigurationTests.java
@@ -38,9 +38,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Phillip Webb
  */
-class ConfigurationPropertiesReportEndpointAutoConfigurationTests {
+public class ConfigurationPropertiesReportEndpointAutoConfigurationTests {
 
-	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+	public final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(ConfigurationPropertiesReportEndpointAutoConfiguration.class));
 
 	@Test
@@ -48,12 +48,6 @@ class ConfigurationPropertiesReportEndpointAutoConfigurationTests {
 		this.contextRunner.withUserConfiguration(Config.class)
 				.withPropertyValues("management.endpoints.web.exposure.include=configprops")
 				.run(validateTestProperties("******", "654321"));
-	}
-
-	@Test
-	void runWhenEnabledPropertyIsFalseShouldNotHaveEndpointBean() {
-		this.contextRunner.withPropertyValues("management.endpoint.configprops.enabled:false")
-				.run((context) -> assertThat(context).doesNotHaveBean(ConfigurationPropertiesReportEndpoint.class));
 	}
 
 	@Test
