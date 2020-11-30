0f5e742c5cb0973f9476f33b007a493a4a7eb1a5
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 04:38:44 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.integrationtest,WebFluxEndpointCorsIntegrationTests,allowedMethodsCanBeConfigured#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/integrationtest/WebFluxEndpointCorsIntegrationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/integrationtest/WebFluxEndpointCorsIntegrationTests.java
@@ -44,9 +44,9 @@ import org.springframework.test.web.reactive.server.WebTestClient;
  * @author Stephane Nicoll
  * @see WebFluxEndpointManagementContextConfiguration
  */
-class WebFluxEndpointCorsIntegrationTests {
+public class WebFluxEndpointCorsIntegrationTests {
 
-	private final ReactiveWebApplicationContextRunner contextRunner = new ReactiveWebApplicationContextRunner()
+	public final ReactiveWebApplicationContextRunner contextRunner = new ReactiveWebApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(JacksonAutoConfiguration.class, CodecsAutoConfiguration.class,
 					WebFluxAutoConfiguration.class, HttpHandlerAutoConfiguration.class, EndpointAutoConfiguration.class,
 					WebEndpointAutoConfiguration.class, ManagementContextAutoConfiguration.class,
@@ -117,17 +117,6 @@ class WebFluxEndpointCorsIntegrationTests {
 	}
 
 	@Test
-	void allowedMethodsCanBeConfigured() {
-		this.contextRunner
-				.withPropertyValues("management.endpoints.web.cors.allowed-origins:spring.example.org",
-						"management.endpoints.web.cors.allowed-methods:GET,HEAD")
-				.run(withWebTestClient((webTestClient) -> webTestClient.options().uri("/actuator/beans")
-						.header("Origin", "spring.example.org")
-						.header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "HEAD").exchange().expectStatus().isOk()
-						.expectHeader().valueEquals(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET,HEAD")));
-	}
-
-	@Test
 	void credentialsCanBeAllowed() {
 		this.contextRunner
 				.withPropertyValues("management.endpoints.web.cors.allowed-origins:spring.example.org",
@@ -145,7 +134,7 @@ class WebFluxEndpointCorsIntegrationTests {
 						.expectHeader().doesNotExist(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS)));
 	}
 
-	private ContextConsumer<ReactiveWebApplicationContext> withWebTestClient(Consumer<WebTestClient> webTestClient) {
+	public ContextConsumer<ReactiveWebApplicationContext> withWebTestClient(Consumer<WebTestClient> webTestClient) {
 		return (context) -> webTestClient.accept(WebTestClient.bindToApplicationContext(context).configureClient()
 				.baseUrl("https://spring.example.org").build());
 	}
