af89aa98420528a5386ea36895a98be372c9755d
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 03:02:21 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.health,AutoConfiguredHealthEndpointGroupTests,showComponentsWhenShowComponentsIsWhenAuthorizedAndUserDoesNotHaveRightAuthoritiesReturnsFalse#,MoveMethod,BUILDFAILED

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/health/AutoConfiguredHealthEndpointGroupTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/health/AutoConfiguredHealthEndpointGroupTests.java
@@ -42,16 +42,17 @@ import static org.mockito.Mockito.mock;
  * @author Phillip Webb
  */
 @ExtendWith(MockitoExtension.class)
+public
 class AutoConfiguredHealthEndpointGroupTests {
 
 	@Mock
-	private StatusAggregator statusAggregator;
+	public StatusAggregator statusAggregator;
 
 	@Mock
-	private HttpCodeStatusMapper httpCodeStatusMapper;
+	public HttpCodeStatusMapper httpCodeStatusMapper;
 
 	@Mock
-	private SecurityContext securityContext;
+	public SecurityContext securityContext;
 
 	@Mock
 	private Principal principal;
@@ -218,18 +219,6 @@ class AutoConfiguredHealthEndpointGroupTests {
 				.willAnswer((invocation) -> Collections.singleton(new SimpleGrantedAuthority("admin")));
 		given(this.securityContext.getPrincipal()).willReturn(principal);
 		assertThat(group.showComponents(this.securityContext)).isTrue();
-	}
-
-	@Test
-	void showComponentsWhenShowComponentsIsWhenAuthorizedAndUserDoesNotHaveRightAuthoritiesReturnsFalse() {
-		AutoConfiguredHealthEndpointGroup group = new AutoConfiguredHealthEndpointGroup((name) -> true,
-				this.statusAggregator, this.httpCodeStatusMapper, Show.WHEN_AUTHORIZED, Show.NEVER,
-				Arrays.asList("admin", "rot", "bossmode"));
-		Authentication principal = mock(Authentication.class);
-		given(principal.getAuthorities())
-				.willAnswer((invocation) -> Collections.singleton(new SimpleGrantedAuthority("other")));
-		given(this.securityContext.getPrincipal()).willReturn(principal);
-		assertThat(group.showComponents(this.securityContext)).isFalse();
 	}
 
 	@Test
