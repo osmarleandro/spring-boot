dc9709926f4ee863fed4b60408c69136dd480be5
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 02:49:14 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.health,AutoConfiguredHealthEndpointGroupTests,showDetailsWhenShowDetailsIsWhenAuthorizedAndUserHasRightAuthorityReturnsTrue#,MoveMethod,BUILDFAILED

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/health/AutoConfiguredHealthEndpointGroupTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/health/AutoConfiguredHealthEndpointGroupTests.java
@@ -42,16 +42,17 @@ import static org.mockito.Mockito.mock;
  * @author Phillip Webb
  */
 @ExtendWith(MockitoExtension.class)
+public
 class AutoConfiguredHealthEndpointGroupTests {
 
 	@Mock
-	private StatusAggregator statusAggregator;
+	public StatusAggregator statusAggregator;
 
 	@Mock
-	private HttpCodeStatusMapper httpCodeStatusMapper;
+	public HttpCodeStatusMapper httpCodeStatusMapper;
 
 	@Mock
-	private SecurityContext securityContext;
+	public SecurityContext securityContext;
 
 	@Mock
 	private Principal principal;
@@ -120,18 +121,6 @@ class AutoConfiguredHealthEndpointGroupTests {
 				Arrays.asList("admin", "root", "bossmode"));
 		given(this.securityContext.getPrincipal()).willReturn(this.principal);
 		assertThat(group.showDetails(this.securityContext)).isFalse();
-	}
-
-	@Test
-	void showDetailsWhenShowDetailsIsWhenAuthorizedAndUserHasRightAuthorityReturnsTrue() {
-		AutoConfiguredHealthEndpointGroup group = new AutoConfiguredHealthEndpointGroup((name) -> true,
-				this.statusAggregator, this.httpCodeStatusMapper, null, Show.WHEN_AUTHORIZED,
-				Arrays.asList("admin", "root", "bossmode"));
-		Authentication principal = mock(Authentication.class);
-		given(principal.getAuthorities())
-				.willAnswer((invocation) -> Collections.singleton(new SimpleGrantedAuthority("admin")));
-		given(this.securityContext.getPrincipal()).willReturn(principal);
-		assertThat(group.showDetails(this.securityContext)).isTrue();
 	}
 
 	@Test
