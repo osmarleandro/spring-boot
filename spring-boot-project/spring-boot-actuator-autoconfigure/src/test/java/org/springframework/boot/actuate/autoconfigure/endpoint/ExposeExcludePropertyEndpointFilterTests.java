09e3bb8d546a522425b73813cc9a5c047ef10b8a
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 00:46:30 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.endpoint,ExposeExcludePropertyEndpointFilterTests,matchWhenExposeIsEmptyAndExcludeIsEmptyAndNotInDefaultShouldNotMatch#,MoveMethod,BUILDFAILED

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/ExposeExcludePropertyEndpointFilterTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/ExposeExcludePropertyEndpointFilterTests.java
@@ -35,6 +35,7 @@ import static org.mockito.Mockito.mock;
  * @author Phillip Webb
  */
 @Deprecated
+public
 class ExposeExcludePropertyEndpointFilterTests {
 
 	private ExposeExcludePropertyEndpointFilter<?> filter;
@@ -71,12 +72,6 @@ class ExposeExcludePropertyEndpointFilterTests {
 	void matchWhenExposeIsEmptyAndExcludeIsEmptyAndInDefaultShouldMatch() {
 		setupFilter("", "");
 		assertThat(match(EndpointId.of("def"))).isTrue();
-	}
-
-	@Test
-	void matchWhenExposeIsEmptyAndExcludeIsEmptyAndNotInDefaultShouldNotMatch() {
-		setupFilter("", "");
-		assertThat(match(EndpointId.of("bar"))).isFalse();
 	}
 
 	@Test
@@ -147,7 +142,7 @@ class ExposeExcludePropertyEndpointFilterTests {
 		assertThat(match(EndpointId.of("bus-refresh"))).isTrue();
 	}
 
-	private void setupFilter(String include, String exclude) {
+	public void setupFilter(String include, String exclude) {
 		MockEnvironment environment = new MockEnvironment();
 		environment.setProperty("foo.include", include);
 		environment.setProperty("foo.exclude", exclude);
@@ -156,7 +151,7 @@ class ExposeExcludePropertyEndpointFilterTests {
 	}
 
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private boolean match(EndpointId id) {
+	public boolean match(EndpointId id) {
 		ExposableEndpoint<?> endpoint = mock(TestExposableWebEndpoint.class);
 		given(endpoint.getEndpointId()).willReturn(id);
 		return ((EndpointFilter) this.filter).match(endpoint);
