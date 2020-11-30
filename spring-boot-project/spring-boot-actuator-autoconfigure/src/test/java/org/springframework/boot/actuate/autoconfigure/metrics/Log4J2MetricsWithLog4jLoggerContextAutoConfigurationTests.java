f9e7a41a2dd9e7a997118cc482f8ea0409d7aeca
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 06:05:53 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.metrics,Log4J2MetricsWithLog4jLoggerContextAutoConfigurationTests,allowsCustomLog4J2MetricsToBeUsed#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/Log4J2MetricsWithLog4jLoggerContextAutoConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/Log4J2MetricsWithLog4jLoggerContextAutoConfigurationTests.java
@@ -35,9 +35,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Andy Wilkinson
  */
 @ClassPathOverrides("org.apache.logging.log4j:log4j-core:2.11.1")
+public
 class Log4J2MetricsWithLog4jLoggerContextAutoConfigurationTests {
 
-	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner().with(MetricsRun.simple())
+	public final ApplicationContextRunner contextRunner = new ApplicationContextRunner().with(MetricsRun.simple())
 			.withConfiguration(AutoConfigurations.of(Log4J2MetricsAutoConfiguration.class));
 
 	@Test
@@ -47,15 +48,8 @@ class Log4J2MetricsWithLog4jLoggerContextAutoConfigurationTests {
 		this.contextRunner.run((context) -> assertThat(context).hasSingleBean(Log4j2Metrics.class));
 	}
 
-	@Test
-	void allowsCustomLog4J2MetricsToBeUsed() {
-		assertThat(LogManager.getContext().getClass().getName())
-				.isEqualTo("org.apache.logging.log4j.core.LoggerContext");
-		this.contextRunner.withUserConfiguration(CustomLog4J2MetricsConfiguration.class).run(
-				(context) -> assertThat(context).hasSingleBean(Log4j2Metrics.class).hasBean("customLog4J2Metrics"));
-	}
-
 	@Configuration(proxyBeanMethods = false)
+	public
 	static class CustomLog4J2MetricsConfiguration {
 
 		@Bean
