79698c04e1cde2c0689d12526d22ac287632ace9
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 03:03:21 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.health,AutoConfiguredHealthEndpointGroupTests,getStatusAggregatorReturnsStatusAggregator#,MoveMethod,BUILDFAILED

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/health/AutoConfiguredHealthEndpointGroupTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/health/AutoConfiguredHealthEndpointGroupTests.java
@@ -42,13 +42,14 @@ import static org.mockito.Mockito.mock;
  * @author Phillip Webb
  */
 @ExtendWith(MockitoExtension.class)
+public
 class AutoConfiguredHealthEndpointGroupTests {
 
 	@Mock
-	private StatusAggregator statusAggregator;
+	public StatusAggregator statusAggregator;
 
 	@Mock
-	private HttpCodeStatusMapper httpCodeStatusMapper;
+	public HttpCodeStatusMapper httpCodeStatusMapper;
 
 	@Mock
 	private SecurityContext securityContext;
@@ -230,13 +231,6 @@ class AutoConfiguredHealthEndpointGroupTests {
 				.willAnswer((invocation) -> Collections.singleton(new SimpleGrantedAuthority("other")));
 		given(this.securityContext.getPrincipal()).willReturn(principal);
 		assertThat(group.showComponents(this.securityContext)).isFalse();
-	}
-
-	@Test
-	void getStatusAggregatorReturnsStatusAggregator() {
-		AutoConfiguredHealthEndpointGroup group = new AutoConfiguredHealthEndpointGroup((name) -> true,
-				this.statusAggregator, this.httpCodeStatusMapper, null, Show.ALWAYS, Collections.emptySet());
-		assertThat(group.getStatusAggregator()).isSameAs(this.statusAggregator);
 	}
 
 	@Test
