7899d6b2a01d5ab6751045e432e9c4eac8a5d657
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-29 21:46:30 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.cloudfoundry.reactive,CloudFoundryReactiveHealthEndpointWebExtensionTests,healthComponentsAlwaysPresent#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/cloudfoundry/reactive/CloudFoundryReactiveHealthEndpointWebExtensionTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/cloudfoundry/reactive/CloudFoundryReactiveHealthEndpointWebExtensionTests.java
@@ -16,19 +16,12 @@
 
 package org.springframework.boot.actuate.autoconfigure.cloudfoundry.reactive;
 
-import java.time.Duration;
-
-import org.junit.jupiter.api.Test;
-
 import org.springframework.boot.actuate.autoconfigure.endpoint.EndpointAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.health.HealthContributorAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.health.HealthEndpointAutoConfiguration;
 import org.springframework.boot.actuate.autoconfigure.web.server.ManagementContextAutoConfiguration;
-import org.springframework.boot.actuate.endpoint.http.ApiVersion;
-import org.springframework.boot.actuate.health.CompositeHealth;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthComponent;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
@@ -40,16 +33,14 @@ import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfigurat
 import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.boot.test.context.runner.ReactiveWebApplicationContextRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 /**
  * Tests for {@link CloudFoundryReactiveHealthEndpointWebExtension}.
  *
  * @author Madhura Bhave
  */
-class CloudFoundryReactiveHealthEndpointWebExtensionTests {
+public class CloudFoundryReactiveHealthEndpointWebExtensionTests {
 
-	private final ReactiveWebApplicationContextRunner contextRunner = new ReactiveWebApplicationContextRunner()
+	public final ReactiveWebApplicationContextRunner contextRunner = new ReactiveWebApplicationContextRunner()
 			.withPropertyValues("VCAP_APPLICATION={}")
 			.withConfiguration(AutoConfigurations.of(ReactiveSecurityAutoConfiguration.class,
 					ReactiveUserDetailsServiceAutoConfiguration.class, WebFluxAutoConfiguration.class,
@@ -61,17 +52,6 @@ class CloudFoundryReactiveHealthEndpointWebExtensionTests {
 					HealthContributorAutoConfiguration.class, HealthEndpointAutoConfiguration.class,
 					ReactiveCloudFoundryActuatorAutoConfiguration.class))
 			.withUserConfiguration(TestHealthIndicator.class);
-
-	@Test
-	void healthComponentsAlwaysPresent() {
-		this.contextRunner.run((context) -> {
-			CloudFoundryReactiveHealthEndpointWebExtension extension = context
-					.getBean(CloudFoundryReactiveHealthEndpointWebExtension.class);
-			HealthComponent body = extension.health(ApiVersion.V3).block(Duration.ofSeconds(30)).getBody();
-			HealthComponent health = ((CompositeHealth) body).getComponents().entrySet().iterator().next().getValue();
-			assertThat(((Health) health).getDetails()).containsEntry("spring", "boot");
-		});
-	}
 
 	private static class TestHealthIndicator implements HealthIndicator {
 
--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/ReactiveWebApplicationContextRunner.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/ReactiveWebApplicationContextRunner.java
@@ -16,9 +16,19 @@
 
 package org.springframework.boot.test.context.runner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
 import java.util.List;
 import java.util.function.Supplier;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.autoconfigure.cloudfoundry.reactive.CloudFoundryReactiveHealthEndpointWebExtension;
+import org.springframework.boot.actuate.autoconfigure.cloudfoundry.reactive.CloudFoundryReactiveHealthEndpointWebExtensionTests;
+import org.springframework.boot.actuate.endpoint.http.ApiVersion;
+import org.springframework.boot.actuate.health.CompositeHealth;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthComponent;
 import org.springframework.boot.context.annotation.Configurations;
 import org.springframework.boot.test.context.assertj.AssertableReactiveWebApplicationContext;
 import org.springframework.boot.test.util.TestPropertyValues;
@@ -77,6 +87,18 @@ public final class ReactiveWebApplicationContextRunner extends
 			List<Configurations> configurations) {
 		return new ReactiveWebApplicationContextRunner(contextFactory, allowBeanDefinitionOverriding, initializers,
 				environmentProperties, systemProperties, classLoader, parent, beanRegistrations, configurations);
+	}
+
+	@Test
+	public
+	void healthComponentsAlwaysPresent(CloudFoundryReactiveHealthEndpointWebExtensionTests cloudFoundryReactiveHealthEndpointWebExtensionTests) {
+		run((context) -> {
+			CloudFoundryReactiveHealthEndpointWebExtension extension = context
+					.getBean(CloudFoundryReactiveHealthEndpointWebExtension.class);
+			HealthComponent body = extension.health(ApiVersion.V3).block(Duration.ofSeconds(30)).getBody();
+			HealthComponent health = ((CompositeHealth) body).getComponents().entrySet().iterator().next().getValue();
+			assertThat(((Health) health).getDetails()).containsEntry("spring", "boot");
+		});
 	}
 
 }
