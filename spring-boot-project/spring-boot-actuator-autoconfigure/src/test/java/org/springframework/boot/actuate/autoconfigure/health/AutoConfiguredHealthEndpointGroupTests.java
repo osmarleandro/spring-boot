2c31e713fcd08813672d9943b1b20f668caa234f
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 02:44:25 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.health,AutoConfiguredHealthEndpointGroupTests,showDetailsWhenShowDetailsIsAlwaysReturnsTrue#,MoveMethod,BUILDFAILED

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/health/AutoConfiguredHealthEndpointGroupTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/health/AutoConfiguredHealthEndpointGroupTests.java
@@ -42,13 +42,14 @@ import static org.mockito.Mockito.mock;
  * @author Phillip Webb
  */
 @ExtendWith(MockitoExtension.class)
+public
 class AutoConfiguredHealthEndpointGroupTests {
 
 	@Mock
-	private StatusAggregator statusAggregator;
+	public StatusAggregator statusAggregator;
 
 	@Mock
-	private HttpCodeStatusMapper httpCodeStatusMapper;
+	public HttpCodeStatusMapper httpCodeStatusMapper;
 
 	@Mock
 	private SecurityContext securityContext;
@@ -77,13 +78,6 @@ class AutoConfiguredHealthEndpointGroupTests {
 		AutoConfiguredHealthEndpointGroup group = new AutoConfiguredHealthEndpointGroup((name) -> true,
 				this.statusAggregator, this.httpCodeStatusMapper, null, Show.NEVER, Collections.emptySet());
 		assertThat(group.showDetails(SecurityContext.NONE)).isFalse();
-	}
-
-	@Test
-	void showDetailsWhenShowDetailsIsAlwaysReturnsTrue() {
-		AutoConfiguredHealthEndpointGroup group = new AutoConfiguredHealthEndpointGroup((name) -> true,
-				this.statusAggregator, this.httpCodeStatusMapper, null, Show.ALWAYS, Collections.emptySet());
-		assertThat(group.showDetails(SecurityContext.NONE)).isTrue();
 	}
 
 	@Test
