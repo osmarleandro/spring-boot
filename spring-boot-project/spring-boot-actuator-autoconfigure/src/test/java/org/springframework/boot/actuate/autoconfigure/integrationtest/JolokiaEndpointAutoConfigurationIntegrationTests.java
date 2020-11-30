30af03289d565db1188fdc63deb94ff547b1e11d
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 04:49:28 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.integrationtest,JolokiaEndpointAutoConfigurationIntegrationTests,jolokiaIsExposed#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/integrationtest/JolokiaEndpointAutoConfigurationIntegrationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/integrationtest/JolokiaEndpointAutoConfigurationIntegrationTests.java
@@ -58,18 +58,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT,
 		properties = "management.endpoints.web.exposure.include=jolokia")
 @DirtiesContext
+public
 class JolokiaEndpointAutoConfigurationIntegrationTests {
 
 	@Autowired
-	private TestRestTemplate restTemplate;
-
-	@Test
-	void jolokiaIsExposed() {
-		ResponseEntity<String> response = this.restTemplate.getForEntity("/actuator/jolokia", String.class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(response.getBody()).contains("\"agent\"");
-		assertThat(response.getBody()).contains("\"request\":{\"type\"");
-	}
+	public TestRestTemplate restTemplate;
 
 	@Test
 	void search() {
