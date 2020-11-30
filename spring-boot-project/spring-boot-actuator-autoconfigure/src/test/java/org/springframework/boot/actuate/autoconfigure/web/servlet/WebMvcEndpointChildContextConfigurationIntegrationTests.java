97d70fad73a34908220c69e51d4018dc4e8a9aa6
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 11:31:02 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.web.servlet,WebMvcEndpointChildContextConfigurationIntegrationTests,errorEndpointIsUsedWithRestControllerEndpoint#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/web/servlet/WebMvcEndpointChildContextConfigurationIntegrationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/web/servlet/WebMvcEndpointChildContextConfigurationIntegrationTests.java
@@ -59,9 +59,9 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Phillip Webb
  * @author Scott Frederick
  */
-class WebMvcEndpointChildContextConfigurationIntegrationTests {
+public class WebMvcEndpointChildContextConfigurationIntegrationTests {
 
-	private final WebApplicationContextRunner runner = new WebApplicationContextRunner(
+	public final WebApplicationContextRunner runner = new WebApplicationContextRunner(
 			AnnotationConfigServletWebServerApplicationContext::new)
 					.withConfiguration(AutoConfigurations.of(ManagementContextAutoConfiguration.class,
 							ServletWebServerFactoryAutoConfiguration.class,
@@ -100,18 +100,6 @@ class WebMvcEndpointChildContextConfigurationIntegrationTests {
 	}
 
 	@Test
-	void errorEndpointIsUsedWithRestControllerEndpoint() {
-		this.runner.run(withWebTestClient((client) -> {
-			Map<String, ?> body = client.get().uri("actuator/failController").accept(MediaType.APPLICATION_JSON)
-					.exchangeToMono(toResponseBody()).block();
-			assertThat(body).hasEntrySatisfying("exception",
-					(value) -> assertThat(value).asString().contains("IllegalStateException"));
-			assertThat(body).hasEntrySatisfying("message",
-					(value) -> assertThat(value).asString().contains("Epic Fail"));
-		}));
-	}
-
-	@Test
 	void errorEndpointIsUsedWithRestControllerEndpointOnBindingError() {
 		this.runner.run(withWebTestClient((client) -> {
 			Map<String, ?> body = client.post().uri("actuator/failController")
@@ -125,7 +113,7 @@ class WebMvcEndpointChildContextConfigurationIntegrationTests {
 		}));
 	}
 
-	private ContextConsumer<AssertableWebApplicationContext> withWebTestClient(Consumer<WebClient> webClient) {
+	public ContextConsumer<AssertableWebApplicationContext> withWebTestClient(Consumer<WebClient> webClient) {
 		return (context) -> {
 			String port = context.getEnvironment().getProperty("local.management.port");
 			WebClient client = WebClient.create("http://localhost:" + port);
@@ -133,7 +121,7 @@ class WebMvcEndpointChildContextConfigurationIntegrationTests {
 		};
 	}
 
-	private Function<ClientResponse, ? extends Mono<Map<String, ?>>> toResponseBody() {
+	public Function<ClientResponse, ? extends Mono<Map<String, ?>>> toResponseBody() {
 		return ((clientResponse) -> clientResponse.bodyToMono(new ParameterizedTypeReference<Map<String, ?>>() {
 		}));
 	}
--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/WebApplicationContextRunner.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/WebApplicationContextRunner.java
@@ -16,15 +16,21 @@
 
 package org.springframework.boot.test.context.runner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.autoconfigure.web.servlet.WebMvcEndpointChildContextConfigurationIntegrationTests;
 import org.springframework.boot.context.annotation.Configurations;
 import org.springframework.boot.test.context.assertj.AssertableWebApplicationContext;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebApplicationContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.web.context.ConfigurableWebApplicationContext;
 import org.springframework.web.context.WebApplicationContext;
@@ -81,6 +87,19 @@ public final class WebApplicationContextRunner extends
 			List<Configurations> configurations) {
 		return new WebApplicationContextRunner(contextFactory, allowBeanDefinitionOverriding, initializers,
 				environmentProperties, systemProperties, classLoader, parent, beanRegistrations, configurations);
+	}
+
+	@Test
+	public
+	void errorEndpointIsUsedWithRestControllerEndpoint(WebMvcEndpointChildContextConfigurationIntegrationTests webMvcEndpointChildContextConfigurationIntegrationTests) {
+		run(webMvcEndpointChildContextConfigurationIntegrationTests.withWebTestClient((client) -> {
+			Map<String, ?> body = client.get().uri("actuator/failController").accept(MediaType.APPLICATION_JSON)
+					.exchangeToMono(webMvcEndpointChildContextConfigurationIntegrationTests.toResponseBody()).block();
+			assertThat(body).hasEntrySatisfying("exception",
+					(value) -> assertThat(value).asString().contains("IllegalStateException"));
+			assertThat(body).hasEntrySatisfying("message",
+					(value) -> assertThat(value).asString().contains("Epic Fail"));
+		}));
 	}
 
 	/**
