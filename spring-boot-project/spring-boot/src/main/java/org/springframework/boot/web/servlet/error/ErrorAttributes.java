0b0542d376dad8cde8735c4176cd5c4592b260d8
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 11:52:34 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.web.servlet,ManagementErrorEndpointTests,errorResponseParamsTrue#,MoveMethod,BUILDFAILED

--- spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/error/ErrorAttributes.java
+++ spring-boot-project/spring-boot/src/main/java/org/springframework/boot/web/servlet/error/ErrorAttributes.java
@@ -16,12 +16,19 @@
 
 package org.springframework.boot.web.servlet.error;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Collections;
 import java.util.Map;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.autoconfigure.web.servlet.ManagementErrorEndpoint;
+import org.springframework.boot.actuate.autoconfigure.web.servlet.ManagementErrorEndpointTests;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.error.ErrorAttributeOptions.Include;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -70,5 +77,19 @@ public interface ErrorAttributes {
 	 * @return the {@link Exception} that caused the error or {@code null}
 	 */
 	Throwable getError(WebRequest webRequest);
+
+	@Test
+	default
+	void errorResponseParamsTrue(ManagementErrorEndpointTests managementErrorEndpointTests) {
+		managementErrorEndpointTests.errorProperties.setIncludeStacktrace(ErrorProperties.IncludeStacktrace.ON_PARAM);
+		managementErrorEndpointTests.errorProperties.setIncludeMessage(ErrorProperties.IncludeAttribute.ON_PARAM);
+		managementErrorEndpointTests.request.addParameter("trace", "true");
+		managementErrorEndpointTests.request.addParameter("message", "true");
+		ManagementErrorEndpoint endpoint = new ManagementErrorEndpoint(this, managementErrorEndpointTests.errorProperties);
+		Map<String, Object> response = endpoint.invoke(new ServletWebRequest(managementErrorEndpointTests.request));
+		assertThat(response).containsEntry("message", "test exception");
+		assertThat(response).hasEntrySatisfying("trace",
+				(value) -> assertThat(value).asString().startsWith("java.lang.RuntimeException: test exception"));
+	}
 
 }
