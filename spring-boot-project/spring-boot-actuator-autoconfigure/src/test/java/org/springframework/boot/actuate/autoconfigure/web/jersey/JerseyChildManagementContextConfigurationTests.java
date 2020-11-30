8b581b1de29f599796231500a13e41be3ccf2098
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-30 11:22:27 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.web.jersey,JerseyChildManagementContextConfigurationTests,jerseyApplicationPathIsAutoConfigured#,MoveMethod,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/web/jersey/JerseyChildManagementContextConfigurationTests.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/web/jersey/JerseyChildManagementContextConfigurationTests.java
@@ -21,7 +21,6 @@ import org.glassfish.jersey.servlet.ServletContainer;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.autoconfigure.AutoConfigurations;
-import org.springframework.boot.autoconfigure.web.servlet.JerseyApplicationPath;
 import org.springframework.boot.test.context.FilteredClassLoader;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
@@ -37,9 +36,10 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Madhura Bhave
  */
 @ClassPathExclusions("spring-webmvc-*")
+public
 class JerseyChildManagementContextConfigurationTests {
 
-	private final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
+	public final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
 			.withUserConfiguration(JerseyChildManagementContextConfiguration.class);
 
 	@Test
@@ -54,14 +54,6 @@ class JerseyChildManagementContextConfigurationTests {
 	void autoConfigurationIsConditionalOnClassResourceConfig() {
 		this.contextRunner.withClassLoader(new FilteredClassLoader(ResourceConfig.class))
 				.run((context) -> assertThat(context).doesNotHaveBean(JerseySameManagementContextConfiguration.class));
-	}
-
-	@Test
-	void jerseyApplicationPathIsAutoConfigured() {
-		this.contextRunner.run((context) -> {
-			JerseyApplicationPath bean = context.getBean(JerseyApplicationPath.class);
-			assertThat(bean.getPath()).isEqualTo("/");
-		});
 	}
 
 	@Test
--- spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/WebApplicationContextRunner.java
+++ spring-boot-project/spring-boot-test/src/main/java/org/springframework/boot/test/context/runner/WebApplicationContextRunner.java
@@ -16,9 +16,14 @@
 
 package org.springframework.boot.test.context.runner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 import java.util.function.Supplier;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.autoconfigure.web.jersey.JerseyChildManagementContextConfigurationTests;
+import org.springframework.boot.autoconfigure.web.servlet.JerseyApplicationPath;
 import org.springframework.boot.context.annotation.Configurations;
 import org.springframework.boot.test.context.assertj.AssertableWebApplicationContext;
 import org.springframework.boot.test.util.TestPropertyValues;
@@ -81,6 +86,15 @@ public final class WebApplicationContextRunner extends
 			List<Configurations> configurations) {
 		return new WebApplicationContextRunner(contextFactory, allowBeanDefinitionOverriding, initializers,
 				environmentProperties, systemProperties, classLoader, parent, beanRegistrations, configurations);
+	}
+
+	@Test
+	public
+	void jerseyApplicationPathIsAutoConfigured(JerseyChildManagementContextConfigurationTests jerseyChildManagementContextConfigurationTests) {
+		run((context) -> {
+			JerseyApplicationPath bean = context.getBean(JerseyApplicationPath.class);
+			assertThat(bean.getPath()).isEqualTo("/");
+		});
 	}
 
 	/**
