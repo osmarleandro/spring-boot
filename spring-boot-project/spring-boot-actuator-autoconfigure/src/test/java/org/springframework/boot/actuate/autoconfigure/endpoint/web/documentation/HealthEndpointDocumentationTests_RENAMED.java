58cc1b4d1ba3c9d069c57821a33a4a614b04d722
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 06:28:28 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.endpoint.web.documentation,HealthEndpointDocumentationTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/documentation/HealthEndpointDocumentationTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/documentation/HealthEndpointDocumentationTests_RENAMED.java
@@ -65,7 +65,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Andy Wilkinson
  * @author Stephane Nicoll
  */
-class HealthEndpointDocumentationTests extends MockMvcEndpointDocumentationTests {
+class HealthEndpointDocumentationTests_RENAMED extends MockMvcEndpointDocumentationTests {
 
 	private static final List<FieldDescriptor> componentFields = Arrays.asList(
 			fieldWithPath("status").description("Status of a specific part of the application"),
