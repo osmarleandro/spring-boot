adbef9c9e4c7ae5d31c5dced6843e057e1b3c7c9
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 07:53:25 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.mongo,MongoHealthContributorAutoConfigurationTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/mongo/MongoHealthContributorAutoConfigurationTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/mongo/MongoHealthContributorAutoConfigurationTests_RENAMED.java
@@ -32,7 +32,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Phillip Webb
  */
-class MongoHealthContributorAutoConfigurationTests {
+class MongoHealthContributorAutoConfigurationTests_RENAMED {
 
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(MongoAutoConfiguration.class, MongoDataAutoConfiguration.class,
