2d7dc26031d27277a307d96674257dd60648c0c8
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 06:11:06 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.cloudfoundry,AccessLevelTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/cloudfoundry/AccessLevelTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/cloudfoundry/AccessLevelTests_RENAMED.java
@@ -25,7 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Madhura Bhave
  */
-class AccessLevelTests {
+class AccessLevelTests_RENAMED {
 
 	@Test
 	void accessToHealthEndpointShouldNotBeRestricted() {
