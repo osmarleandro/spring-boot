44e549c7ffff40f9b97b1935b907c419ca4453f8
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 07:19:53 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.metrics.export.datadog,DatadogMetricsExportAutoConfigurationTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/datadog/DatadogMetricsExportAutoConfigurationTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/datadog/DatadogMetricsExportAutoConfigurationTests_RENAMED.java
@@ -34,7 +34,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
-class DatadogMetricsExportAutoConfigurationTests {
+class DatadogMetricsExportAutoConfigurationTests_RENAMED {
 
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(DatadogMetricsExportAutoConfiguration.class));
