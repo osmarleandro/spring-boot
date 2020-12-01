43f375b5026c63289d5a2eb2b41856f5c03a7dee
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 08:04:06 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.web,ManagementContextConfigurationTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/web/ManagementContextConfigurationTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/web/ManagementContextConfigurationTests_RENAMED.java
@@ -29,7 +29,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
-class ManagementContextConfigurationTests {
+class ManagementContextConfigurationTests_RENAMED {
 
 	@Test
 	void proxyBeanMethodsIsEnabledByDefault() {
