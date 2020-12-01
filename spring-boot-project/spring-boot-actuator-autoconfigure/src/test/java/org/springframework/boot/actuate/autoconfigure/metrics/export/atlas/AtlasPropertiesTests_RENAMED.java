92177779d9b66051d22d4482fbd092793afb0e70
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 07:17:44 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.metrics.export.atlas,AtlasPropertiesTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/atlas/AtlasPropertiesTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/atlas/AtlasPropertiesTests_RENAMED.java
@@ -26,7 +26,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Stephane Nicoll
  */
-class AtlasPropertiesTests {
+class AtlasPropertiesTests_RENAMED {
 
 	@Test
 	void defaultValuesAreConsistent() {
