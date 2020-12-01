7ea8f6d5d0cc8a493dc354500ace3ff902913928
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 07:04:10 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.management,ThreadDumpEndpointAutoConfigurationTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/management/ThreadDumpEndpointAutoConfigurationTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/management/ThreadDumpEndpointAutoConfigurationTests_RENAMED.java
@@ -29,7 +29,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Phillip Webb
  */
-class ThreadDumpEndpointAutoConfigurationTests {
+class ThreadDumpEndpointAutoConfigurationTests_RENAMED {
 
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(ThreadDumpEndpointAutoConfiguration.class));
