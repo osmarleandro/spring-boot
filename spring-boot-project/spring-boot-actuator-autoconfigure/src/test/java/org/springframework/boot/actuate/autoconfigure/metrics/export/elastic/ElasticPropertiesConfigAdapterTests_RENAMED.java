9d715449847965b12c2eaec627df01ba7727b961
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 07:22:57 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.metrics.export.elastic,ElasticPropertiesConfigAdapterTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/elastic/ElasticPropertiesConfigAdapterTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/elastic/ElasticPropertiesConfigAdapterTests_RENAMED.java
@@ -25,7 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
-class ElasticPropertiesConfigAdapterTests {
+class ElasticPropertiesConfigAdapterTests_RENAMED {
 
 	@Test
 	void whenPropertiesHostsIsSetAdapterHostsReturnsIt() {
