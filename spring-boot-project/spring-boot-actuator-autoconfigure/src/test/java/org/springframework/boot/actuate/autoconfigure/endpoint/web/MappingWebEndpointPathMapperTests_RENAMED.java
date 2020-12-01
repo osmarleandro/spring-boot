71f00b0c3ee4880affadb35fabfea217542dfe1d
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 06:28:13 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.endpoint.web,MappingWebEndpointPathMapperTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/MappingWebEndpointPathMapperTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/MappingWebEndpointPathMapperTests_RENAMED.java
@@ -30,7 +30,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Stephane Nicoll
  */
-class MappingWebEndpointPathMapperTests {
+class MappingWebEndpointPathMapperTests_RENAMED {
 
 	@Test
 	void defaultConfiguration() {
