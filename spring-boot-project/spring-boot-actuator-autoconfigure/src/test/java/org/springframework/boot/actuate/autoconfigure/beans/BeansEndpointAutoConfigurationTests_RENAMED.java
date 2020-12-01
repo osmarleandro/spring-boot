89d3850c14a35ffec3429081667242a14e2ea5fe
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 06:09:21 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.beans,BeansEndpointAutoConfigurationTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/beans/BeansEndpointAutoConfigurationTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/beans/BeansEndpointAutoConfigurationTests_RENAMED.java
@@ -29,7 +29,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Phillip Webb
  */
-class BeansEndpointAutoConfigurationTests {
+class BeansEndpointAutoConfigurationTests_RENAMED {
 
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(BeansEndpointAutoConfiguration.class));
