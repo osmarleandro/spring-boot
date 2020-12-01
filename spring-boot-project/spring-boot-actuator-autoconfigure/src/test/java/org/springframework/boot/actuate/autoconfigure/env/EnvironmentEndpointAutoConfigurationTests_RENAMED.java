a7c6b705f40ff35b171579bad3945a70883cb043
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 06:43:22 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.env,EnvironmentEndpointAutoConfigurationTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/env/EnvironmentEndpointAutoConfigurationTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/env/EnvironmentEndpointAutoConfigurationTests_RENAMED.java
@@ -36,7 +36,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Phillip Webb
  */
-class EnvironmentEndpointAutoConfigurationTests {
+class EnvironmentEndpointAutoConfigurationTests_RENAMED {
 
 	private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
 			.withConfiguration(AutoConfigurations.of(EnvironmentEndpointAutoConfiguration.class));
