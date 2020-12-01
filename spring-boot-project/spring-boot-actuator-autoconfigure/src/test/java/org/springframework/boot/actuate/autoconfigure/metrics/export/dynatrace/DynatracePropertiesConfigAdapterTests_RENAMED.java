bd9934159b705904d2b5392d960143f5ae032dd1
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 07:21:48 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.metrics.export.dynatrace,DynatracePropertiesConfigAdapterTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/dynatrace/DynatracePropertiesConfigAdapterTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/metrics/export/dynatrace/DynatracePropertiesConfigAdapterTests_RENAMED.java
@@ -25,7 +25,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  * @author Andy Wilkinson
  */
-class DynatracePropertiesConfigAdapterTests {
+class DynatracePropertiesConfigAdapterTests_RENAMED {
 
 	@Test
 	void whenPropertiesUriIsSetAdapterUriReturnsIt() {
