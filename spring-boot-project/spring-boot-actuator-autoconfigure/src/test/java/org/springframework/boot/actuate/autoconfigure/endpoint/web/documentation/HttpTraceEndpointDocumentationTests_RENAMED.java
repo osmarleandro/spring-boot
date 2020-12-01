a021e8692c0a4b084c2c41cccbb2fca61b51f458
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-01 06:35:05 -0300

    spring-boot-actuator-autoconfigure,org.springframework.boot.actuate.autoconfigure.endpoint.web.documentation,HttpTraceEndpointDocumentationTests,NA,RenameType,OK

--- spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/documentation/HttpTraceEndpointDocumentationTests_RENAMED.java
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/documentation/HttpTraceEndpointDocumentationTests_RENAMED.java
@@ -52,7 +52,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  *
  * @author Andy Wilkinson
  */
-class HttpTraceEndpointDocumentationTests extends MockMvcEndpointDocumentationTests {
+class HttpTraceEndpointDocumentationTests_RENAMED extends MockMvcEndpointDocumentationTests {
 
 	@MockBean
 	private HttpTraceRepository repository;
