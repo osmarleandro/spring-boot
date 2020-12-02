2793e0774d66facf1c5ed0161887b460b43e671f
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-02 08:36:23 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.web.annotation,AbstractWebEndpointIntegrationTests,readOperationWithSingleQueryParametersAndMultipleValues#,PushDownMethod,OK,false,false,0

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/reactive/WebFluxEndpointIntegrationTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/web/reactive/WebFluxEndpointIntegrationTests.java
@@ -100,6 +100,12 @@ class WebFluxEndpointIntegrationTests
 		return context.getBean(ReactiveConfiguration.class).port;
 	}
 
+	@Test
+	void readOperationWithSingleQueryParametersAndMultipleValues() {
+		load(QueryEndpointConfiguration.class, (client) -> client.get().uri("/query?one=1&one=1&two=2").exchange()
+				.expectStatus().isOk().expectBody().jsonPath("query").isEqualTo("1,1 2"));
+	}
+
 	@Configuration(proxyBeanMethods = false)
 	@EnableWebFlux
 	@ImportAutoConfiguration(ErrorWebFluxAutoConfiguration.class)
