6b90f94205a771d874e18d1614455b362df48459
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-16 20:45:04 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.web,WebOperationRequestPredicate,hashCode#,ExtractMethod,OK,false

--- spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/WebOperationRequestPredicate.java
+++ spring-boot-project/spring-boot-actuator/src/main/java/org/springframework/boot/actuate/endpoint/web/WebOperationRequestPredicate.java
@@ -137,11 +137,16 @@ public final class WebOperationRequestPredicate {
 	@Override
 	public int hashCode() {
 		final int prime = 31;
-		int result = 1;
-		result = prime * result + this.consumes.hashCode();
+		int result = extracted(prime);
 		result = prime * result + this.httpMethod.hashCode();
 		result = prime * result + this.canonicalPath.hashCode();
 		result = prime * result + this.produces.hashCode();
+		return result;
+	}
+
+	private int extracted(final int prime) {
+		int result = 1;
+		result = prime * result + this.consumes.hashCode();
 		return result;
 	}
 
