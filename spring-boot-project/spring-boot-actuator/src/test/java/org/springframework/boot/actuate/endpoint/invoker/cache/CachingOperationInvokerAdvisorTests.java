7303d2ee4d442333220820d5910921ddfed3d88e
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-17 06:12:02 -0300

    spring-boot-actuator,org.springframework.boot.actuate.endpoint.invoker.cache,CachingOperationInvokerAdvisorTests,applyWhenTimeToLiveIsZeroShouldNotAddAdvise#,ExtractMethod,OK,false

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/invoker/cache/CachingOperationInvokerAdvisorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/endpoint/invoker/cache/CachingOperationInvokerAdvisorTests.java
@@ -90,12 +90,17 @@ class CachingOperationInvokerAdvisorTests {
 
 	@Test
 	void applyWhenTimeToLiveIsZeroShouldNotAddAdvise() {
-		OperationParameters parameters = getParameters("get");
-		given(this.timeToLive.apply(any())).willReturn(0L);
+		OperationParameters parameters = extracted();
 		OperationInvoker advised = this.advisor.apply(EndpointId.of("foo"), OperationType.READ, parameters,
 				this.invoker);
 		assertThat(advised).isSameAs(this.invoker);
 		verify(this.timeToLive).apply(EndpointId.of("foo"));
+	}
+
+	private OperationParameters extracted() {
+		OperationParameters parameters = getParameters("get");
+		given(this.timeToLive.apply(any())).willReturn(0L);
+		return parameters;
 	}
 
 	@Test
