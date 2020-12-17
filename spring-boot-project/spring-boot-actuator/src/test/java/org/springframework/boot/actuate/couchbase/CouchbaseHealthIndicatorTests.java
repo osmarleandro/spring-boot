4fc409650965f47e7800441da194dfe933f4b1fb
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-17 03:28:04 -0300

    spring-boot-actuator,org.springframework.boot.actuate.couchbase,CouchbaseHealthIndicatorTests,couchbaseClusterIsDown#,ExtractMethod,OK,false

--- spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/couchbase/CouchbaseHealthIndicatorTests.java
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/couchbase/CouchbaseHealthIndicatorTests.java
@@ -68,6 +68,16 @@ class CouchbaseHealthIndicatorTests {
 	@SuppressWarnings("unchecked")
 	void couchbaseClusterIsDown() {
 		Cluster cluster = mock(Cluster.class);
+		CouchbaseHealthIndicator healthIndicator = extracted(cluster);
+		Health health = healthIndicator.health();
+		assertThat(health.getStatus()).isEqualTo(Status.DOWN);
+		assertThat(health.getDetails()).containsEntry("sdk", "test-sdk");
+		assertThat(health.getDetails()).containsKey("endpoints");
+		assertThat((List<Map<String, Object>>) health.getDetails().get("endpoints")).hasSize(2);
+		verify(cluster).diagnostics();
+	}
+
+	private CouchbaseHealthIndicator extracted(Cluster cluster) {
 		CouchbaseHealthIndicator healthIndicator = new CouchbaseHealthIndicator(cluster);
 		Map<ServiceType, List<EndpointDiagnostics>> endpoints = Collections.singletonMap(ServiceType.KV,
 				Arrays.asList(
@@ -77,12 +87,7 @@ class CouchbaseHealthIndicatorTests {
 								Optional.empty(), Optional.of(1234L), Optional.of("endpoint-2"))));
 		DiagnosticsResult diagnostics = new DiagnosticsResult(endpoints, "test-sdk", "test-id");
 		given(cluster.diagnostics()).willReturn(diagnostics);
-		Health health = healthIndicator.health();
-		assertThat(health.getStatus()).isEqualTo(Status.DOWN);
-		assertThat(health.getDetails()).containsEntry("sdk", "test-sdk");
-		assertThat(health.getDetails()).containsKey("endpoints");
-		assertThat((List<Map<String, Object>>) health.getDetails().get("endpoints")).hasSize(2);
-		verify(cluster).diagnostics();
+		return healthIndicator;
 	}
 
 }
